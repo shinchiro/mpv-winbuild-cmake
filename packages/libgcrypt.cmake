ef06c92a711ab8fac3c77101e08e96e1a3a3dd4d
Martin Herkt <lachs0r@srsfckn.biz>
2013-10-19 20:07:42 +0200

    libgcrypt: Fix stpcpy check

--- packages/libgcrypt.cmake
+++ packages/libgcrypt.cmake
@@ -7,6 +7,7 @@ ExternalProject_Add(libgcrypt
     URL "ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.5.3.tar.bz2"
     URL_MD5 993159b2924ae7b0e4eaff0743c2db35
     PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/libgcrypt-0001-Various-fixes-for-MinGW-w64.patch
+        COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/libgcrypt-0002-Fix-stpcpy-check-on-MinGW-w64.patch
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
@@ -18,3 +19,5 @@ ExternalProject_Add(libgcrypt
         COMMAND ${CMAKE_COMMAND} -E create_symlink ${MINGW_INSTALL_PREFIX}/bin/libgcrypt-config ${CMAKE_INSTALL_PREFIX}/bin/libgcrypt-config
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
+
+autoreconf(libgcrypt)
