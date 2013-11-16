ecb2c8e0241e6850f8adfd2afb9830ad0c26b597
Martin Herkt <lachs0r@srsfckn.biz>
2013-11-16 17:39:25 +0100

    Revert "libgcrypt: Fix stpcpy check"
    No longer necessary.
    
    This reverts commit ef06c92a711ab8fac3c77101e08e96e1a3a3dd4d.

--- packages/libgcrypt.cmake
+++ packages/libgcrypt.cmake
@@ -7,7 +7,6 @@ ExternalProject_Add(libgcrypt
     URL "ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.5.3.tar.bz2"
     URL_MD5 993159b2924ae7b0e4eaff0743c2db35
     PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/libgcrypt-0001-Various-fixes-for-MinGW-w64.patch
-        COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/libgcrypt-0002-Fix-stpcpy-check-on-MinGW-w64.patch
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
@@ -19,5 +18,3 @@ ExternalProject_Add(libgcrypt
         COMMAND ${CMAKE_COMMAND} -E create_symlink ${MINGW_INSTALL_PREFIX}/bin/libgcrypt-config ${CMAKE_INSTALL_PREFIX}/bin/libgcrypt-config
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
-
-autoreconf(libgcrypt)
