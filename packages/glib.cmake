24d449288df0d79b2f25639e73d704469f51d24e
Martin Herkt <lachs0r@srsfckn.biz>
2014-01-15 13:51:31 +0100

    Re-add glib monstrosity
    Without gio for now.

---
+++ packages/glib.cmake
@@ -0,0 +1,33 @@
+ExternalProject_Add(glib
+    DEPENDS gettext libffi pcre
+    GIT_REPOSITORY "git://git.gnome.org/glib"
+    GIT_TAG 2.39.3
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/glib-*.patch
+    CONFIGURE_COMMAND ${EXEC}
+        <SOURCE_DIR>/configure
+        --host=${TARGET_ARCH}
+        --prefix=${MINGW_INSTALL_PREFIX}
+        --disable-shared
+        --enable-static
+        --with-threads=win32
+        --with-pcre=system
+        --with-libiconv=gnu
+        --disable-inotify
+        CXX=${TARGET_ARCH}-c++
+    BUILD_COMMAND ${MAKE} -C glib
+        COMMAND ${MAKE} -C gmodule
+        COMMAND ${MAKE} -C gthread
+        COMMAND ${MAKE} -C gobject
+#         COMMAND ${MAKE} -C gio
+    INSTALL_COMMAND ${MAKE} -C glib install
+        COMMAND ${MAKE} -C gmodule install
+        COMMAND ${MAKE} -C gthread install
+        COMMAND ${MAKE} -C gobject install
+#         COMMAND ${MAKE} -C gio install
+        COMMAND ${MAKE} install-pkgconfigDATA
+        COMMAND ${MAKE} -C m4macros install
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+autogen(glib)
