cced15c1b29cc120e1bf1da09781b62d07a3711d
Martin Herkt <lachs0r@srsfckn.biz>
2014-01-15 18:17:05 +0100

    libquvi-scripts: update to 0.9 series

--- packages/libquvi_scripts.cmake
+++ packages/libquvi_scripts.cmake
@@ -1,18 +1,20 @@
 ExternalProject_Add(libquvi_scripts
     DEPENDS luajit
     GIT_REPOSITORY "git://repo.or.cz/libquvi-scripts.git"
-    GIT_TAG v0.4.21
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/libquvi_scripts-*.patch
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
         --with-nsfw
         --with-fixme
-        --with-nlfy
+        --without-manual
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
     BUILD_IN_SOURCE 1
 )
 
-autogen(libquvi_scripts)
+force_rebuild_git(libquvi_scripts)
+autoreconf(libquvi_scripts)
