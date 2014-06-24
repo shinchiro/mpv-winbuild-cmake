a910e518fb4c43085769f1e5cce77fa197946c47
Martin Herkt <lachs0r@srsfckn.biz>
2014-06-24 05:34:43 +0200

    fix luaexpat

--- packages/luaexpat.cmake
+++ packages/luaexpat.cmake
@@ -1,14 +1,19 @@
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/luaexpat-config.in
-               ${CMAKE_CURRENT_BINARY_DIR}/luaexpat-config
-               @ONLY)
-
-set(LUAEXPAT_MFLAGS CONFIG=${CMAKE_CURRENT_BINARY_DIR}/luaexpat-config)
+set(LUAEXPAT_MFLAGS
+    LUA_CDIR=${MINGW_INSTALL_PREFIX}/lua/5.1
+    LUA_LDIR=${MINGW_INSTALL_PREFIX}/lua/5.1/lua
+    LUA_INC=-I${MINGW_INSTALL_PREFIX}/include/luajit-2.0
+    EXPAT_INC=-I${MINGW_INSTALL_PREFIX}/include
+    CC=${TARGET_ARCH}-gcc
+    "COMMON_LDFLAGS='-s -shared'"
+    LDFLAGS=-lluajit-5.1
+    LIBNAME=lxp.dll
+)
 
 ExternalProject_Add(luaexpat
     DEPENDS luajit expat
     GIT_REPOSITORY "https://github.com/LuaDist/luaexpat.git"
     UPDATE_COMMAND ""
-    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/luaexpat-*.patch
+#     PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/luaexpat-*.patch
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${MAKE} ${LUAEXPAT_MFLAGS}
     INSTALL_COMMAND ${MAKE} ${LUAEXPAT_MFLAGS} install
