046ffc8a46cfa9877596da5119ea04b0e4ddb123
Martin Herkt <lachs0r@srsfckn.biz>
2014-08-17 00:59:28 +0200

    freetype2: fix install error with CMake 3.x

--- packages/freetype2.cmake
+++ packages/freetype2.cmake
@@ -12,7 +12,7 @@ ExternalProject_Add(freetype2
         --with-sysroot=${MINGW_INSTALL_PREFIX}
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
-        COMMAND ${CMAKE_COMMAND} -E create_symlink ${MINGW_INSTALL_PREFIX}/bin/freetype-config ${CMAKE_INSTALL_PREFIX}/bin
+        COMMAND ${CMAKE_COMMAND} -E create_symlink ${MINGW_INSTALL_PREFIX}/bin/freetype-config ${CMAKE_INSTALL_PREFIX}/bin/freetype-config
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
