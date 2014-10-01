d933213218dc298972fc0e163b19ceac465cac1f
Martin Herkt <lachs0r@srsfckn.biz>
2014-10-01 17:42:57 +0200

    fribidi: update, --enable-malloc
    Seriously what the FUCK is wrong with people. Why has this
    “chunked memory” bullshit been implemented and why is it the
    default instead of simple malloc? It’s not even thread-safe.

--- packages/fribidi.cmake
+++ packages/fribidi.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(fribidi
     DEPENDS gcc
-    URL "http://fribidi.org/download/fribidi-0.19.5.tar.bz2"
-    URL_MD5 925bafb97afee8a2fc2d0470c072a155
+    URL "http://fribidi.org/download/fribidi-0.19.6.tar.bz2"
+    URL_HASH SHA256=cba8b7423c817e5adf50d28ec9079d14eafcec9127b9e8c8f1960c5ad585e17d
     PATCH_COMMAND sed -i "s,__declspec(dllimport),," "lib/fribidi-common.h"
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
@@ -10,6 +10,7 @@ ExternalProject_Add(fribidi
         --disable-deprecated
         --without-glib
         --enable-charsets
+        --enable-malloc
     BUILD_COMMAND ${MAKE} -j1 # breaks with parallel make
     INSTALL_COMMAND ${MAKE} -j1 install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
