42924b96a07b8dda0c82e884d8cc4add85ec667a
Martin Herkt <lachs0r@srsfckn.biz>
2014-11-05 19:17:27 +0100

    Update libgpg_error

--- packages/libgpg_error.cmake
+++ packages/libgpg_error.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(libgpg_error
     DEPENDS gcc
-    URL "ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.12.tar.bz2"
-    URL_MD5 8f0eb41a344d19ac2aa9bd101dfb9ce6
+    URL "ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.17.tar.bz2"
+    URL_HASH SHA256=3ff4e5a71116eb862cd14185fcd282850927b8608e3b4186834fd940fbef57b5
     PATCH_COMMAND patch -p0 < ${CMAKE_CURRENT_SOURCE_DIR}/libgpg_error-0001-Fix-potomo-hang-on-Cygwin.patch
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
