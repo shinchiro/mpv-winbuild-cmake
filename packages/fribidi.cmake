15f645255e4e9bfe917638fc19108c65aa9dac27
Martin Herkt <lachs0r@srsfckn.biz>
2015-01-27 08:24:55 +0100

    FriBidi: remove superfluous patch step

--- packages/fribidi.cmake
+++ packages/fribidi.cmake
@@ -2,7 +2,6 @@ ExternalProject_Add(fribidi
     DEPENDS gcc
     URL "http://fribidi.org/download/fribidi-0.19.6.tar.bz2"
     URL_HASH SHA256=cba8b7423c817e5adf50d28ec9079d14eafcec9127b9e8c8f1960c5ad585e17d
-    PATCH_COMMAND sed -i "s,__declspec(dllimport),," "lib/fribidi-common.h"
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
