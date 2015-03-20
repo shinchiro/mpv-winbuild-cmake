0af644f922e7287525671516250c906e2325dbea
Martin Herkt <lachs0r@srsfckn.biz>
2015-03-20 03:04:23 +0100

    game-music-emu: switch to Bitbucket, remove patches
    Project got moved from Google Code. I took the opportunity to fix
    the static build problem upstream.

--- packages/game-music-emu.cmake
+++ packages/game-music-emu.cmake
@@ -1,13 +1,11 @@
 ExternalProject_Add(game-music-emu
     DEPENDS gcc
-    SVN_REPOSITORY "http://game-music-emu.googlecode.com/svn/trunk/"
+    GIT_REPOSITORY "https://bitbucket.org/mpyne/game-music-emu.git"
     UPDATE_COMMAND ""
-    PATCH_COMMAND patch -p0 < ${CMAKE_CURRENT_SOURCE_DIR}/game-music-emu-0001-static.patch
-        COMMAND patch -p0 < ${CMAKE_CURRENT_SOURCE_DIR}/game-music-emu-0002-Add-Libs.private.patch
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX} -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DLIBTYPE=STATIC
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX} -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE} -DBUILD_SHARED_LIBS=OFF
     BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
     INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-force_rebuild_svn(game-music-emu)
+force_rebuild_git(game-music-emu)
