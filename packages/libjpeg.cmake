363371b1ebff70ed811b1a51b10b90b28aac51c8
Martin Herkt <lachs0r@srsfckn.biz>
2015-07-30 21:37:16 +0200

    libjpeg: switch to mozjpeg; work around typedef clash

--- packages/libjpeg.cmake
+++ packages/libjpeg.cmake
@@ -1,7 +1,8 @@
 ExternalProject_Add(libjpeg
     DEPENDS gcc
-    URL "http://download.sourceforge.net/libjpeg-turbo/libjpeg-turbo-1.4.0.tar.gz"
-    URL_HASH SHA256=d93ad8546b510244f863b39b4c0da0fa4c0d53a77b61a8a3880f258c232bbbee
+    GIT_REPOSITORY "https://github.com/mozilla/mozjpeg.git"
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/libjpeg-*.patch
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
@@ -10,3 +11,6 @@ ExternalProject_Add(libjpeg
     INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
+
+force_rebuild_git(libjpeg)
+autoreconf(libjpeg)
