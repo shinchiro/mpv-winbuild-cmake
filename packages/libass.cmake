df3e109a4505cd435cab9c24a8a4319671a86239
Martin Herkt <lachs0r@srsfckn.biz>
2015-07-30 21:52:34 +0200

    libass: use dwrite instead of fontconfig on 64-bit

--- packages/libass.cmake
+++ packages/libass.cmake
@@ -1,11 +1,24 @@
+if(${TARGET_CPU} MATCHES "i686")
+    set(ASS_USE_FONTCONFIG fontconfig)
+else()
+    set(ASS_DISABLE_FONTCONFIG "--disable-fontconfig")
+endif()
+
 ExternalProject_Add(libass
-    DEPENDS harfbuzz freetype2 fontconfig fribidi enca
+    DEPENDS
+        harfbuzz
+        freetype2
+        ${ASS_USE_FONTCONFIG}
+        fribidi
+        enca
     GIT_REPOSITORY "https://github.com/libass/libass.git"
+    GIT_TAG fonts
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
+        ${ASS_DISABLE_FONTCONFIG}
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
