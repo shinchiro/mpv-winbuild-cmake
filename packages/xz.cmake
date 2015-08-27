2636e883e36244a9092f7ba32919391b125eedf5
Martin Herkt <lachs0r@srsfckn.biz>
2015-08-27 22:14:51 +0200

    add xz

---
+++ packages/xz.cmake
@@ -0,0 +1,20 @@
+ExternalProject_Add(xz
+    DEPENDS gcc
+    GIT_REPOSITORY "http://git.tukaani.org/xz.git"
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
+        --host=${TARGET_ARCH}
+        --prefix=${MINGW_INSTALL_PREFIX}
+        --disable-shared
+        --disable-xz
+        --disable-xzdec
+        --disable-lzmadec
+        --disable-lzmainfo
+        --disable-doc
+    BUILD_COMMAND ${MAKE}
+    INSTALL_COMMAND ${MAKE} install
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+force_rebuild_git(xz)
+autogen(xz)
