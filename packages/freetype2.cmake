0992096aff60ab8a0cf0a7df758fab640262e6ae
shinchiro <ihsanapps@gmail.com>
2015-11-02 14:37:36 +0800

    Compile freetype2 without harfbuzz to avoid circular dependency issue
    harfbuzz support in freetype is not really important to libass.
    
    https://github.com/libass/libass/issues/199#issuecomment-145508550

--- packages/freetype2.cmake
+++ packages/freetype2.cmake
@@ -1,7 +1,7 @@
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/ft2exec.in ${CMAKE_CURRENT_BINARY_DIR}/ft2exec)
 
 ExternalProject_Add(freetype2
-    DEPENDS libpng zlib libpng
+    DEPENDS libpng zlib
     GIT_REPOSITORY git://git.sv.gnu.org/freetype/freetype2.git
     GIT_DEPTH 1
     UPDATE_COMMAND ""
@@ -10,6 +10,7 @@ ExternalProject_Add(freetype2
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
+        --without-harfbuzz
         --with-sysroot=${MINGW_INSTALL_PREFIX}
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
