266978afc44f518b290be5b10bda28cff08e6f54
shinchiro <ihsanapps@gmail.com>
2015-12-31 00:08:34 +0800

    opus: fix previous commit

--- packages/opus.cmake
+++ packages/opus.cmake
@@ -3,7 +3,8 @@ ExternalProject_Add(opus
     GIT_REPOSITORY git://git.xiph.org/opus.git
     GIT_DEPTH 1
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
+    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/autogen.sh
+    COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
