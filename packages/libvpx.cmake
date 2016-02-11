d2f2ee760d0f46d115ac19dfd94665049f06e580
shinchiro <ihsanapps@gmail.com>
2016-02-11 11:18:45 +0800

    libvpx: remove patch
    fixed in upstream

--- packages/libvpx.cmake
+++ packages/libvpx.cmake
@@ -9,7 +9,6 @@ ExternalProject_Add(libvpx
     GIT_REPOSITORY https://chromium.googlesource.com/webm/libvpx.git
     GIT_DEPTH 1
     UPDATE_COMMAND ""
-    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/libvpx-*.patch
     CONFIGURE_COMMAND ${EXEC} CROSS=${TARGET_ARCH}- <SOURCE_DIR>/configure
         --target=${libvpx_target}
         --prefix=${MINGW_INSTALL_PREFIX}
