986f359e9e5148159d537dc09730865bee7198a1
shinchiro <shinchiro@users.noreply.github.com>
2016-09-24 23:33:18 +0800

    angle: build with gyp
    instead of relying on handwritten makefile

--- packages/angle.cmake
+++ packages/angle.cmake
@@ -1,20 +1,56 @@
 ExternalProject_Add(angle
-    DEPENDS 
+    DEPENDS
         ffmpeg
     GIT_REPOSITORY https://chromium.googlesource.com/angle/angle
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
     PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/angle-*.patch
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${MAKE} clean
+    CONFIGURE_COMMAND gyp -Duse_ozone=0 -DOS=win -Dangle_gl_library_type=static_library
+        -Dangle_use_commit_id=1 --depth . -I build/common.gypi src/angle.gyp --no-parallel
+        --format=make --generator-output=generated
+    BUILD_COMMAND ""
     INSTALL_COMMAND ${MAKE}
-        CXX=${TARGET_ARCH}-g++
-        AR=${TARGET_ARCH}-ar
-        RANLIB=${TARGET_ARCH}-ranlib
         PREFIX=${MINGW_INSTALL_PREFIX}
         install
     BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
+ExternalProject_Add_Step(angle make-commitid
+    DEPENDEES configure
+    WORKING_DIRECTORY <SOURCE_DIR>/generated
+    COMMAND ${MAKE} commit_id
+)
+
+ExternalProject_Add_Step(angle copy-commitid
+    DEPENDEES make-commitid
+    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/generated/out/Debug/obj/gen/angle/id/commit.h <SOURCE_DIR>/src/id/commit.h
+)
+
+ExternalProject_Add_Step(angle make-all
+    DEPENDEES copy-commitid
+    WORKING_DIRECTORY <SOURCE_DIR>/generated
+    COMMAND ${MAKE}
+        CXX=${TARGET_ARCH}-g++
+        AR=${TARGET_ARCH}-ar
+        RANLIB=${TARGET_ARCH}-ranlib
+        BUILDTYPE=Release
+    LOG 1
+)
+
+ExternalProject_Add_Step(angle move-libs
+    DEPENDEES make-all
+    DEPENDERS install
+    COMMAND ${EXEC} bash <SOURCE_DIR>/move-libs.sh ${TARGET_ARCH}
+)
+
+ExternalProject_Add_Step(angle clean-buildfiles
+   DEPENDEES move-libs
+   COMMAND rm -R <SOURCE_DIR>/generated
+)
+
 force_rebuild_git(angle)
+
+# This is too confusing
+# DEPENDEES: Steps on which this step depends
+# DEPENDERS: Steps that depend on this step
