0f92f3d6230c92a86054bb202af507a3cff30918
shinchiro <shinchiro@users.noreply.github.com>
2016-10-03 17:54:02 +0800

    opus: clean source dir before compile

--- packages/opus.cmake
+++ packages/opus.cmake
@@ -13,5 +13,13 @@ ExternalProject_Add(opus
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
+ExternalProject_Add_Step(opus clean
+    DEPENDERS update
+    WORKING_DIRECTORY <SOURCE_DIR>
+    COMMAND git clean -dfx
+    ALWAYS 1
+    LOG 1
+)
+
 clean_build_dir(opus)
 force_rebuild_git(opus)
