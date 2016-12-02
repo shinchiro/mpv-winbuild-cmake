15f0cf982389136a5fb20c76b41a9a5788872632
shinchiro <shinchiro@users.noreply.github.com>
2016-12-02 10:02:15 +0800

    Add vapoursynth package
    By default, it does not automatically build with 'ninja mpv' command

---
+++ packages/vapoursynth.cmake
@@ -0,0 +1,21 @@
+ExternalProject_Add(vapoursynth
+    GIT_REPOSITORY https://github.com/vapoursynth/vapoursynth.git
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/vapoursynth-*.patch
+    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
+        --host=${TARGET_ARCH}
+        --prefix=${MINGW_INSTALL_PREFIX}
+        --enable-core
+        --disable-python-module
+        --disable-vsscript
+        --disable-plugins
+        --disable-shared
+    BUILD_COMMAND ${MAKE}
+    INSTALL_COMMAND ${MAKE} install
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+clean_build_dir(vapoursynth)
+force_rebuild_git(vapoursynth)
+autogen(vapoursynth)
