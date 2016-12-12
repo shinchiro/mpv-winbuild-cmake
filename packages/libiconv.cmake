3155fe119229005d06b2ae9146b7d5f5967d6547
shinchiro <shinchiro@users.noreply.github.com>
2016-12-12 23:10:46 +0800

    libiconv: fix commit a500015
    fix linking error in ffmpeg on msys

--- packages/libiconv.cmake
+++ packages/libiconv.cmake
@@ -1,14 +1,13 @@
 if(CYGWIN OR MSYS)
-    set(target --build=${TARGET_ARCH})
-else()
-    set(target --host=${TARGET_ARCH})
+    set(build --build=${TARGET_ARCH})
 endif()
 
 ExternalProject_Add(libiconv
     URL "http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz"
     URL_MD5 e34509b1623cec449dfeb73d7ce9c6c6
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-        ${target}
+        ${build}
+        --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-nls
         --disable-shared
