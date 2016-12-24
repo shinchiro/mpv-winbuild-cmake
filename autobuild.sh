dc3d98cc6d8db22863e94def62130bbb65adb4f1
shinchiro <shinchiro@users.noreply.github.com>
2016-12-24 16:21:46 +0800

    autobuild: Don't compile 64-bit mpv if 32-bit failed to compile

--- autobuild.sh
+++ autobuild.sh
@@ -7,6 +7,13 @@ cmake -DTARGET_ARCH=i686-w64-mingw32 -G Ninja ..
 ninja mpv
 cd ..
 
+if [ -d ./build32/mpv-* ] ; then
+    echo "Successfully compiled 32-bit. Continue"
+else
+    echo "Failed compiled 32-bit. Stop"
+    exit
+fi
+
 mkdir -p ./build64
 cd ./build64
 cmake -DTARGET_ARCH=x86_64-w64-mingw32 -G Ninja ..
