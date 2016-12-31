a71a9fab86f9b8c5d52f2d0c4952efeee121a969
shinchiro <shinchiro@users.noreply.github.com>
2016-12-31 17:43:41 +0800

    mpv-rename: more verbose git info

--- packages/mpv-rename.sh
+++ packages/mpv-rename.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# Append commit hash at the end of filename
+# Append tag, commit's length and hash in folder name
 # $1 = git_dir $2 = folder_name
 cd $1
-GIT=$(git rev-parse --short @{u})
-mv $2 $2-git-${GIT}
+git_revision=$(git rev-parse --short @{u})
+tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+length=$(git rev-list --count $tag..$git_revision)
+git_revision="git-$git_revision"
+version="$tag+$length-$git_revision"
+
+mv $2 $2-${version}
