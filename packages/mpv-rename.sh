767a07556944b32786f5c4a25f34c1b806bd43f2
shinchiro <shinchiro@users.noreply.github.com>
2017-01-02 09:36:47 +0800

    mpv: revert back using built date when packaging

--- packages/mpv-rename.sh
+++ packages/mpv-rename.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-# Append tag, commit's length and hash in folder name
+# Append commit hash at the end of filename
 # $1 = git_dir $2 = folder_name
 cd $1
-git_revision=$(git rev-parse --short @{u})
-tag=$(git describe --tags $(git rev-list --tags --max-count=1))
-length=$(git rev-list --count $tag..$git_revision)
-git_revision="git-$git_revision"
-version="$tag+$length-$git_revision"
-
-mv $2 $2-${version}
+GIT=$(git rev-parse --short @{u})
+mv $2 $2-git-${GIT}
