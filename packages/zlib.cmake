6c85dddf52f24cbbc9cbaebe6500bf26a0f24a56
shinchiro <shinchiro@users.noreply.github.com>
2017-01-02 10:00:40 +0800

    zlib: update to v1.2.9

--- packages/zlib.cmake
+++ packages/zlib.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(zlib
-    URL "http://zlib.net/zlib-1.2.8.tar.gz"
-    URL_MD5 44d667c142d7cda120332623eab69f40
+    URL "http://zlib.net/zlib-1.2.9.tar.gz"
+    URL_HASH SHA256=73ab302ef31ed1e74895d2af56f52f5853f26b0370f3ef21954347acec5eaa21
     PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/zlib-1-win32-static.patch
     CONFIGURE_COMMAND ${EXEC} CHOST=${TARGET_ARCH} <SOURCE_DIR>/configure
         --prefix=${MINGW_INSTALL_PREFIX}
