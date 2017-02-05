45b847610f31bcd1040815fed7981fdf0de74a0c
shinchiro <shinchiro@users.noreply.github.com>
2017-02-05 19:58:05 +0800

    zlib: update to v1.2.11 and switch to alternate url

--- packages/zlib.cmake
+++ packages/zlib.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(zlib
-    URL "http://zlib.net/zlib-1.2.10.tar.gz"
-    URL_HASH SHA256=8d7e9f698ce48787b6e1c67e6bff79e487303e66077e25cb9784ac8835978017
+    URL "http://download.sourceforge.net/libpng/zlib-1.2.11.tar.gz"
+    URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
     PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/zlib-1-win32-static.patch
     CONFIGURE_COMMAND ${EXEC} CHOST=${TARGET_ARCH} <SOURCE_DIR>/configure
         --prefix=${MINGW_INSTALL_PREFIX}
