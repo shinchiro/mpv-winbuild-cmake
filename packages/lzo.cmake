2868a145cbf6f0aadf2c1e5e14e0863828f311ac
myfreeer <myfreeer@users.noreply.github.com>
2017-05-22 17:17:28 +0800

    lzo: use mirror (#22)

--- packages/lzo.cmake
+++ packages/lzo.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add(lzo
-    URL "http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz"
+    URL "https://fossies.org/linux/misc/lzo-2.10.tar.gz"
     URL_HASH SHA1=4924676a9bae5db58ef129dc1cebce3baa3c4b5d
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
