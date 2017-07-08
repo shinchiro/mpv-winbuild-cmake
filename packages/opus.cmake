5a9dce30c211847c16aa7640f1ba79f4afa4efbe
shinchiro <shinchiro@users.noreply.github.com>
2017-07-08 13:41:56 +0800

    opus: disable shallow clone
    Full clone is necessary for versioning

--- packages/opus.cmake
+++ packages/opus.cmake
@@ -1,6 +1,5 @@
 ExternalProject_Add(opus
     GIT_REPOSITORY https://github.com/xiph/opus.git
-    GIT_SHALLOW 1
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/autogen.sh
     COMMAND ${EXEC} <SOURCE_DIR>/configure
