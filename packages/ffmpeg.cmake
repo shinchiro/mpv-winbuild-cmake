0f6f39bbf827a60b832066bff84b50c9ebfec832
shinchiro <shinchiro@users.noreply.github.com>
2017-07-13 23:05:54 +0800

    ffmpeg: drop patch
    fixed on upstream

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -22,7 +22,6 @@ ExternalProject_Add(ffmpeg
     GIT_SHALLOW 1
     DOWNLOAD_NAME "ffmpeg"
     UPDATE_COMMAND ""
-    PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/ffmpeg-0001-Use-pkg-config-for-more-external-libs.patch
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
     --cross-prefix=${TARGET_ARCH}-
     --prefix=${MINGW_INSTALL_PREFIX}
