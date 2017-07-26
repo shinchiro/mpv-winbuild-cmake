bcb49bfaa2da2bf47fd1f90db4f091481888949e
myfreeer <myfreeer@users.noreply.github.com>
2017-07-26 15:57:44 +0800

    libjpeg: update to 1.5.2

--- packages/libjpeg.cmake
+++ packages/libjpeg.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(libjpeg
-    URL "http://download.sourceforge.net/libjpeg-turbo/libjpeg-turbo-1.5.1.tar.gz"
-    URL_HASH SHA256=41429d3d253017433f66e3d472b8c7d998491d2f41caa7306b8d9a6f2a2c666c
+    URL "http://download.sourceforge.net/libjpeg-turbo/libjpeg-turbo-1.5.2.tar.gz"
+    URL_HASH SHA256=9098943b270388727ae61de82adec73cf9f0dbb240b3bc8b172595ebf405b528
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
