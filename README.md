936938d4be6916655b86cc21768697496fb5a833
shinchiro <shinchiro@users.noreply.github.com>
2017-11-19 17:10:39 +0800

    fribidi: switch to git

--- README.md
+++ README.md
@@ -54,6 +54,7 @@ Alternatively, you can download the builds from [here](https://sourceforge.net/p
     - shaderc
     - vulkan
     - crossc
+    - fribidi
 
 - Zip
     - expat (2.2.5)
@@ -67,7 +68,6 @@ Alternatively, you can download the builds from [here](https://sourceforge.net/p
     - libmodplug (0.8.9.0)
     - libiconv (1.15)
     - gmp (6.1.2)
-    - fribidi (0.19.7)
 
 
 ## Setup Build Environment
--- packages/fribidi.cmake
+++ packages/fribidi.cmake
@@ -1,16 +1,17 @@
 ExternalProject_Add(fribidi
-    URL "http://fribidi.org/download/fribidi-0.19.7.tar.bz2"
-    URL_HASH SHA256=08222a6212bbc2276a2d55c3bf370109ae4a35b689acbc66571ad2a670595a8e
+    GIT_REPOSITORY https://github.com/fribidi/fribidi.git
+    GIT_SHALLOW 1
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
+        --enable-static
         --disable-deprecated
         --without-glib
         --enable-charsets
-        --enable-malloc
-    BUILD_COMMAND ${MAKE} -j1 # breaks with parallel make
-    INSTALL_COMMAND ${MAKE} -j1 install
+    BUILD_COMMAND ${MAKE}
+    INSTALL_COMMAND ${MAKE} install
+    BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
