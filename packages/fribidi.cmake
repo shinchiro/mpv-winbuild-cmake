06f4dbd2d77cf08c6b98c45f027bcd83d84c1660
shinchiro <shinchiro@users.noreply.github.com>
2017-11-25 11:53:57 +0800

    fribidi: fix compiling on real linux platform
    WSL, cygwin, msys doesn't need wine to execute .exe file

--- packages/fribidi.cmake
+++ packages/fribidi.cmake
@@ -1,7 +1,14 @@
+if(CMAKE_SYSTEM_NAME MATCHES "Linux" AND NOT CMAKE_SYSTEM_VERSION MATCHES "Microsoft")
+    find_program(WINE wine)
+    set(TARGET_EXEC "TARGET_EXEC=${WINE}")
+endif()
+
 ExternalProject_Add(fribidi
     GIT_REPOSITORY https://github.com/fribidi/fribidi.git
     GIT_SHALLOW 1
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/fribidi-*.patch
+    CONFIGURE_COMMAND ${EXEC} PKG_CONFIG=pkg-config <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
@@ -9,8 +16,8 @@ ExternalProject_Add(fribidi
         --disable-deprecated
         --without-glib
         --enable-charsets
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+    BUILD_COMMAND ${MAKE} -j1 ${TARGET_EXEC} # breaks with parallel make
+    INSTALL_COMMAND ${MAKE} -j1 install
     BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
