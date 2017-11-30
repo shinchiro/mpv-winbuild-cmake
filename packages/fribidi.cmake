d74d7047414d5ae23cddc50e01fc7ed9425bdceb
shinchiro <shinchiro@users.noreply.github.com>
2017-11-30 14:27:56 +0800

    fribidi: try another cross-compile hack
    Get rid of wine's dependency. Should work on msys/cygwin, I guess

--- packages/fribidi.cmake
+++ packages/fribidi.cmake
@@ -1,8 +1,3 @@
-if(CMAKE_SYSTEM_NAME MATCHES "Linux" AND NOT CMAKE_SYSTEM_VERSION MATCHES "Microsoft")
-    find_program(WINE wine)
-    set(TARGET_EXEC "TARGET_EXEC=${WINE}")
-endif()
-
 ExternalProject_Add(fribidi
     GIT_REPOSITORY https://github.com/fribidi/fribidi.git
     GIT_SHALLOW 1
@@ -16,7 +11,7 @@ ExternalProject_Add(fribidi
         --disable-deprecated
         --without-glib
         --enable-charsets
-    BUILD_COMMAND ${MAKE} -j1 ${TARGET_EXEC} # breaks with parallel make
+    BUILD_COMMAND ${MAKE} -j1 # breaks with parallel make
     INSTALL_COMMAND ${MAKE} -j1 install
     BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
