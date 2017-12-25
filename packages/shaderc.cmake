b5aac0bb31cff6a9f9d51f3013f0248dee6f7d38
shinchiro <shinchiro@users.noreply.github.com>
2017-12-25 14:49:07 +0800

    shaderc: copy lib without rename
    Removed 'fno-rtti' flag too

--- packages/shaderc.cmake
+++ packages/shaderc.cmake
@@ -8,7 +8,6 @@ ExternalProject_Add(shaderc
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_TOOLCHAIN_FILE=<SOURCE_DIR>/cmake/linux-mingw-toolchain.cmake
         -DSHADERC_SKIP_TESTS=ON
-        -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -fno-rtti'
         -DMINGW_COMPILER_PREFIX=${TARGET_ARCH}
     BUILD_COMMAND ${MAKE} -i -C <SOURCE_DIR>/build
     INSTALL_COMMAND ""
@@ -27,7 +26,7 @@ ExternalProject_Add_Step(shaderc pull
 ExternalProject_Add_Step(shaderc manual-install
     DEPENDEES build
     COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/libshaderc/include/shaderc ${MINGW_INSTALL_PREFIX}/include/shaderc
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/libshaderc/libshaderc_combined.a ${MINGW_INSTALL_PREFIX}/lib/libshaderc_shared.a
+    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/libshaderc/libshaderc_combined.a ${MINGW_INSTALL_PREFIX}/lib/libshaderc_combined.a
     COMMENT "Manually installing"
 )
 
