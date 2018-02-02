41b30ef1155da5e1a77dfb40b36735fb7b1d9fdb
shinchiro <shinchiro@users.noreply.github.com>
2018-02-02 15:32:16 +0800

    lame: replace git url
    Fix #59

--- packages/lame.cmake
+++ packages/lame.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add(lame
-    GIT_REPOSITORY git://anonscm.debian.org/pkg-multimedia/lame.git
+    GIT_REPOSITORY https://salsa.debian.org/multimedia-team/lame.git
     UPDATE_COMMAND ""
     PATCH_COMMAND ${DEBPATCH}
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
