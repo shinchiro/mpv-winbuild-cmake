33003c1db1bdc1a5fcc50bc0dd6cda84c9074f24
shinchiro <shinchiro@users.noreply.github.com>
2018-03-10 15:51:04 +0800

    binutils: add patch
    This will fix segfault in some program when using binutils 2.30

--- toolchain/binutils.cmake
+++ toolchain/binutils.cmake
@@ -1,6 +1,7 @@
 ExternalProject_Add(binutils
     URL https://sourceware.org/pub/binutils/releases/binutils-2.30.tar.xz
     URL_HASH SHA256=6e46b8aeae2f727a36f0bd9505e405768a72218f1796f0d09757d45209871ae6
+    PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/binutils-0001-remove-provide-qualifiers.patch
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
         --target=${TARGET_ARCH}
         --prefix=${CMAKE_INSTALL_PREFIX}
