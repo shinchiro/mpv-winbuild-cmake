32d222ec5fbe59d200c2890eb0c5101d9466e435
shinchiro <shinchiro@users.noreply.github.com>
2018-03-23 10:22:24 +0800

    libjpeg: changed to cmake build

--- packages/libjpeg.cmake
+++ packages/libjpeg.cmake
@@ -1,15 +1,16 @@
 ExternalProject_Add(libjpeg
     GIT_REPOSITORY https://github.com/libjpeg-turbo/libjpeg-turbo.git
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DENABLE_SHARED=OFF
+        -DENABLE_STATIC=ON
+        -DCMAKE_BUILD_TYPE=Release
+    BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
+    INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(libjpeg)
 extra_step(libjpeg)
-autoreconf(libjpeg)
