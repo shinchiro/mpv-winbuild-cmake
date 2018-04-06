0df267b16f3953abbbacfa2b19b3720bd630aa6a
shinchiro <shinchiro@users.noreply.github.com>
2018-04-06 19:35:14 +0800

    update-repo: exclude updating aom
    doesn't worth since it still in early days.
    Also excluding megasdk and angle. Nobody cares about them anyway

--- scripts/update-repo.sh
+++ scripts/update-repo.sh
@@ -6,8 +6,13 @@ main() {
         local name=$(echo $(basename $dir) | sed -e 's|-prefix$||')
         local src_dir=$packages_dir/$name-prefix/src/$name
         local stamp_dir=$packages_dir/$name-prefix/src/$name-stamp
-        
+
         if [[ -d "$src_dir/.git" ]] ; then
+            # Skip updating these packages
+            if [[ $name =~ ^(angle|megasdk|aom)$ ]]; then
+                continue
+            fi
+
             gitupdate $name $src_dir $stamp_dir &
         # TODO: handle hg repo
         fi
@@ -23,10 +28,10 @@ gitupdate()
 
     echo "Updating $name"
     git -C $src_dir reset --hard @{u} > /dev/null
-    
+
     result=$(git -C $src_dir pull 2>&1)
     result_module=$(git -C $src_dir submodule update --remote --recursive 2>&1)
-    
+
     if [[ ! "$result" =~ up[-\ ]to[-\ ]date ]] || [[ ! -z $result_module ]]; then
         echo "Deleting stamp files for $name"
         find $stamp_dir -maxdepth 1 -type f ! -iname "*.cmake" -size 0c -delete # remove stamp files to force rebuild
