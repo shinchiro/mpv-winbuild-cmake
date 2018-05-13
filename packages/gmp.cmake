93f9c2f50ec80ced132c5e91ace99e12ea7ad462
shinchiro <shinchiro@users.noreply.github.com>
2018-05-13 12:12:32 +0800

    gmp: fix configure error

--- packages/gmp.cmake
+++ packages/gmp.cmake
@@ -2,8 +2,10 @@ ExternalProject_Add(gmp
     URL "ftp://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2"
     URL_HASH SHA256=5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
+        CC_FOR_BUILD=cc
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
+        --enable-static
         --disable-shared
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
