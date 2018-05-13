88d080342a017d8f33aee49db8ae52e00c551d85
shinchiro <shinchiro@users.noreply.github.com>
2018-05-13 12:56:26 +0800

    libvpx: fix build error
    Introduced in gcc 8

--- packages/libvpx.cmake
+++ packages/libvpx.cmake
@@ -9,6 +9,7 @@ ExternalProject_Add(libvpx
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CROSS=${TARGET_ARCH}- <SOURCE_DIR>/configure
+        --extra-cflags='-fno-asynchronous-unwind-tables'
         --target=${libvpx_target}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-examples
