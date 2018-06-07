be368ef4dad7f9177e0ee1697df040908fd4f941
myfreeer <myfreeer@users.noreply.github.com>
2018-06-07 12:29:17 +0800

    vulkan: update repo
    The `Vulkan-LoaderAndValidationLayers` repo has been deprecated
    and splitted into 4 repos, in which `Vulkan-Headers` and
    `Vulkan-Loader` was used by mpv.
    
    Ref: https://github.com/KhronosGroup/Vulkan-LoaderAndValidationLayers/commit/25d5884746a2de7b51a8ef3ec88e1cd8066460e8

--- packages/vulkan.cmake
+++ packages/vulkan.cmake
@@ -1,11 +1,28 @@
+ExternalProject_Add(vulkan-header
+    GIT_REPOSITORY https://github.com/KhronosGroup/Vulkan-Headers.git
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_INSTALL 1
+)
+
+force_rebuild_git(vulkan-header)
+extra_step(vulkan-header)
+
 ExternalProject_Add(vulkan
-    GIT_REPOSITORY git://github.com/KhronosGroup/Vulkan-LoaderAndValidationLayers.git
+    DEPENDS vulkan-header
+    GIT_REPOSITORY https://github.com/KhronosGroup/Vulkan-Loader.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
     PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-*.patch
     CONFIGURE_COMMAND ${EXEC} cmake
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DVULKAN_HEADERS_INSTALL_DIR=${MINGW_INSTALL_PREFIX}
         ##
         -DCMAKE_SYSTEM_NAME=Windows
         -DCMAKE_C_COMPILER=${TARGET_ARCH}-gcc
@@ -13,28 +30,16 @@ ExternalProject_Add(vulkan
         -DCMAKE_RC_COMPILER=${TARGET_ARCH}-windres
         -DCMAKE_AR_COMPILER=${TARGET_ARCH}-ar
         -DCMAKE_RANLIB_COMPILER=${TARGET_ARCH}-ranlib
+        -DCMAKE_ASM-ATT_COMPILER=${TARGET_ARCH}-as
         -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS} -D_WIN32_WINNT=0x0600 -D__STDC_FORMAT_MACROS'
         -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -D__USE_MINGW_ANSI_STDIO -D__STDC_FORMAT_MACROS -fpermissive -D_WIN32_WINNT=0x0600'
         ##
-        -DBUILD_ICD=OFF
-        -DBUILD_DEMOS=OFF
         -DBUILD_TESTS=OFF
-        -DBUILD_LAYERS=OFF
-        -DBUILD_VKJSON=OFF
-    BUILD_COMMAND ${MAKE} -C loader
+        -DENABLE_STATIC_LOADER=ON
     BUILD_IN_SOURCE 1
-    INSTALL_COMMAND ""
+    BUILD_COMMAND ${MAKE}
+    INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-
-)
-
-ExternalProject_Add_Step(vulkan manual-install
-    DEPENDEES build
-    WORKING_DIRECTORY <SOURCE_DIR>
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include/vulkan ${MINGW_INSTALL_PREFIX}/include/vulkan
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/loader/libvulkan.a ${MINGW_INSTALL_PREFIX}/lib/libvulkan.a
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/loader/vulkan.pc ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/vulkan.pc
-    COMMENT "Manual installing"
 )
 
 force_rebuild_git(vulkan)
