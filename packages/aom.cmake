ea156d8af34e71b72513ad58dfa4eccd209a7312
shinchiro <shinchiro@users.noreply.github.com>
2018-07-14 23:10:28 +0800

    aom: change build thing
    Suprisingly fixed problem where aom doesn't detect ar linker for unknown reason

--- packages/aom.cmake
+++ packages/aom.cmake
@@ -2,7 +2,7 @@ ExternalProject_Add(aom
     GIT_REPOSITORY https://aomedia.googlesource.com/aom
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
-    CMAKE_ARGS
+    CONFIGURE_COMMAND ${EXEC} cmake -H. -B<SOURCE_DIR>/aom_build
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
         -DCMAKE_FIND_ROOT_PATH=${CMAKE_INSTALL_PREFIX}
         -DCMAKE_TOOLCHAIN_FILE=<SOURCE_DIR>/build/cmake/toolchains/${TARGET_CPU_FAMILY}-mingw-gcc.cmake
@@ -15,8 +15,9 @@ ExternalProject_Add(aom
         -DENABLE_TESTDATA=OFF
         -DCONFIG_UNIT_TESTS=0
         -DCMAKE_BUILD_TYPE=Release
-    BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
-    INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND ${MAKE} -C <SOURCE_DIR>/aom_build
+    INSTALL_COMMAND ${MAKE} -C <SOURCE_DIR>/aom_build install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
