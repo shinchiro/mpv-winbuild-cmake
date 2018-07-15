f457e584a393bc9aba75cdbf539a98a188bd963d
shinchiro <shinchiro@users.noreply.github.com>
2018-07-15 19:48:57 +0800

    shaderc: cosmetics changes

--- packages/shaderc.cmake
+++ packages/shaderc.cmake
@@ -5,14 +5,13 @@ ExternalProject_Add(shaderc
         spirv-tools
     GIT_REPOSITORY https://github.com/google/shaderc.git
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} cmake -H. -B<SOURCE_DIR>/build
+    CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_TOOLCHAIN_FILE=<SOURCE_DIR>/cmake/linux-mingw-toolchain.cmake
         -DSHADERC_SKIP_TESTS=ON
         -DMINGW_COMPILER_PREFIX=${TARGET_ARCH}
-    BUILD_COMMAND ${MAKE} -C <SOURCE_DIR>/build
+    BUILD_COMMAND ${MAKE} -C <BINARY_DIR>
     INSTALL_COMMAND ""
-    BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
@@ -29,7 +28,7 @@ ExternalProject_Add_Step(shaderc symlink
 ExternalProject_Add_Step(shaderc manual-install
     DEPENDEES build
     COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/libshaderc/include/shaderc ${MINGW_INSTALL_PREFIX}/include/shaderc
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/libshaderc/libshaderc_combined.a ${MINGW_INSTALL_PREFIX}/lib/libshaderc_combined.a
+    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/libshaderc/libshaderc_combined.a ${MINGW_INSTALL_PREFIX}/lib/libshaderc_combined.a
     COMMENT "Manually installing"
 )
 
