48ef6f976f9173142f094c76e3bd42985fb6e8cc
shinchiro <shinchiro@users.noreply.github.com>
2018-09-29 12:39:05 +0800

    fribidi: change to meson build

--- packages/fribidi.cmake
+++ packages/fribidi.cmake
@@ -2,22 +2,17 @@ ExternalProject_Add(fribidi
     GIT_REPOSITORY https://github.com/fribidi/fribidi.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
-    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/fribidi-*.patch
-    CONFIGURE_COMMAND ${EXEC} PKG_CONFIG=pkg-config <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
+    CONFIGURE_COMMAND ${EXEC} meson <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-        --enable-static
-        --disable-deprecated
-        --without-glib
-        --enable-charsets
-        --disable-docs
-    BUILD_COMMAND ${MAKE} -j1 # breaks with parallel make
-    INSTALL_COMMAND ${MAKE} -j1 install
-    BUILD_IN_SOURCE 1
+        --cross-file=${MESON_CROSS}
+        --buildtype=release
+        --default-library=static
+        -Ddeprecated=false
+        -Ddocs=false
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(fribidi)
 extra_step(fribidi)
-autoreconf(fribidi)
