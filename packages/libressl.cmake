5168fa2f4ef7c20c33d0874de2f8697e94017f62
shinchiro <shinchiro@users.noreply.github.com>
2018-10-06 17:26:43 +0800

    libressl: switch to cmake & ninja build

--- packages/libressl.cmake
+++ packages/libressl.cmake
@@ -1,17 +1,19 @@
 ExternalProject_Add(libressl
     GIT_REPOSITORY https://github.com/libressl-portable/portable.git
     GIT_SHALLOW 1
-    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/libressl-*.patch
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+    CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+        -G Ninja
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DLIBRESSL_APPS=OFF
+        -DLIBRESSL_TESTS=OFF
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(libressl)
-autogen(libressl)
 extra_step(libressl)
+autogen(libressl)
