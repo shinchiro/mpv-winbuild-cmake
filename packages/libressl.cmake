baf1755c37d0aaf7b10d7d3248d6e39bd38dae10
shinchiro <shinchiro@users.noreply.github.com>
2018-12-17 22:14:38 +0800

    libressl: apply temporary patch to fix broken build

--- packages/libressl.cmake
+++ packages/libressl.cmake
@@ -1,6 +1,7 @@
 ExternalProject_Add(libressl
     GIT_REPOSITORY https://github.com/libressl-portable/portable.git
     GIT_SHALLOW 1
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/libressl-*.patch
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
