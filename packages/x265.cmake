076ea6bd4d26d008fe730c575c8ada15926aa374
shinchiro <shinchiro@users.noreply.github.com>
2018-12-29 00:37:41 +0800

    x265: fix properly building x265 multilibs

--- packages/x265.cmake
+++ packages/x265.cmake
@@ -8,95 +8,94 @@ ExternalProject_Add(x265-base
 
 get_property(source_dir TARGET x265-base PROPERTY _EP_SOURCE_DIR)
 
-ExternalProject_Add(x265-12bit
-    DEPENDS
-        x265-base
-    DOWNLOAD_COMMAND ""
-    UPDATE_COMMAND ""
-    SOURCE_DIR ${source_dir}
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} <SOURCE_DIR>/source
-        -DCMAKE_INSTALL_PREFIX=<BINARY_DIR> -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
-        -DHIGH_BIT_DEPTH=ON -DMAIN12=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF
-    BUILD_COMMAND ${MAKE}
-    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-)
-
 ExternalProject_Add(x265-10bit
     DEPENDS
         x265-base
     DOWNLOAD_COMMAND ""
     UPDATE_COMMAND ""
-    SOURCE_DIR ${source_dir}
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} <SOURCE_DIR>/source
+    CONFIGURE_COMMAND ${EXEC} cmake -H${source_dir}/source -B<BINARY_DIR> -G Ninja
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX} -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DHIGH_BIT_DEPTH=ON -DENABLE_SHARED=OFF -DSTATIC_LINK_CRT=ON
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+ExternalProject_Add(x265-12bit-lib
+    DEPENDS
+        x265-base
+    DOWNLOAD_COMMAND ""
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ${EXEC} cmake -H${source_dir}/source -B<BINARY_DIR> -G Ninja
+        -DCMAKE_INSTALL_PREFIX=<BINARY_DIR> -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DHIGH_BIT_DEPTH=ON -DMAIN12=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+ExternalProject_Add(x265-10bit-lib
+    DEPENDS
+        x265-base
+        x265-12bit-lib
+    DOWNLOAD_COMMAND ""
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ${EXEC} cmake -H${source_dir}/source -B<BINARY_DIR> -G Ninja
         -DCMAKE_INSTALL_PREFIX=<BINARY_DIR> -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
         -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF
-    BUILD_COMMAND ${MAKE}
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ""
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-ExternalProject_Add(x265
+ExternalProject_Add(x265-multilibs
     DEPENDS
         x265-base
-        x265-12bit
-        x265-10bit
+        x265-12bit-lib
+        x265-10bit-lib
     DOWNLOAD_COMMAND ""
     UPDATE_COMMAND ""
-    SOURCE_DIR ${source_dir}
-    PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/x265-0001-extra-libs.patch
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} <SOURCE_DIR>/source
+    LIST_SEPARATOR ^^
+    CONFIGURE_COMMAND ${EXEC} cmake -H${source_dir}/source -B<BINARY_DIR> -G Ninja
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX} -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
-        -DEXTRA_LINK_FLAGS=-L. -DLINKED_10BIT=ON -DLINKED_12BIT=ON
-    BUILD_COMMAND ${MAKE}
+        -DEXTRA_LIB='x265_main10.a^^x265_main12.a' -DEXTRA_LINK_FLAGS=-L. -DLINKED_10BIT=ON -DLINKED_12BIT=ON
+        -DENABLE_SHARED=OFF
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install/strip
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-ExternalProject_Add(x265-10bit-single
-    DEPENDS
-        x265-base
-    DOWNLOAD_COMMAND ""
-    UPDATE_COMMAND ""
-    SOURCE_DIR ${source_dir}
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} <SOURCE_DIR>/source
-        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX} -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
-        -DHIGH_BIT_DEPTH=ON -DENABLE_SHARED=OFF -DSTATIC_LINK_CRT=1
-        #-DNATIVE_BUILD=ON
-    BUILD_COMMAND ${MAKE}
-    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-)
+get_property(multilibs_dir TARGET x265-multilibs PROPERTY _EP_BINARY_DIR)
 
-get_property(x265_binary_dir TARGET x265 PROPERTY _EP_BINARY_DIR)
-
-ExternalProject_Add_Step(x265-12bit copy-lib
+ExternalProject_Add_Step(x265-12bit-lib copy-lib
     DEPENDEES build
-    DEPENDERS install
     WORKING_DIRECTORY <BINARY_DIR>
-    COMMAND ${CMAKE_COMMAND} -E copy libx265.a ${x265_binary_dir}/libx265_main12.a
+    COMMAND ${CMAKE_COMMAND} -E copy libx265.a ${multilibs_dir}/libx265_main12.a
     ALWAYS 1
 )
 
-ExternalProject_Add_Step(x265-10bit copy-lib
+ExternalProject_Add_Step(x265-10bit-lib copy-lib
     DEPENDEES build
-    DEPENDERS install
     WORKING_DIRECTORY <BINARY_DIR>
-    COMMAND ${CMAKE_COMMAND} -E copy libx265.a ${x265_binary_dir}/libx265_main10.a
+    COMMAND ${CMAKE_COMMAND} -E copy libx265.a ${multilibs_dir}/libx265_main10.a
     ALWAYS 1
 )
 
-ExternalProject_Add_Step(x265 rename-lib
+ExternalProject_Add_Step(x265-multilibs rename-lib
     DEPENDEES build
-    WORKING_DIRECTORY ${x265_binary_dir}
+    WORKING_DIRECTORY ${multilibs_dir}
     COMMAND ${CMAKE_COMMAND} -E copy libx265.a libx265_main.a
 )
 
-ExternalProject_Add_Step(x265 combine-libs
+ExternalProject_Add_Step(x265-multilibs combine-libs
     DEPENDEES build rename-lib
-    DEPENDERS install
-    WORKING_DIRECTORY ${x265_binary_dir}
+    WORKING_DIRECTORY ${multilibs_dir}
     COMMAND libtool --mode=link g++ -static -o libx265.a libx265_main.a libx265_main10.a libx265_main12.a
+    LOG 1
 )
 
-extra_step(x265-12bit)
-extra_step(x265-10bit)
-extra_step(x265)
 extra_step(x265-base)
-extra_step(x265-10bit-single)
+extra_step(x265-12bit-lib)
+extra_step(x265-10bit-lib)
+extra_step(x265-multilibs)
+extra_step(x265-10bit)
