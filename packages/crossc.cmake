c8312814520155a87e12795920810ccdb18c5574
shinchiro <shinchiro@users.noreply.github.com>
2019-01-21 00:24:56 +0800

    crossc: revert commit cb50a06 & e5c8ba2
    Doesn't works well in long term.

--- packages/crossc.cmake
+++ packages/crossc.cmake
@@ -1,6 +1,4 @@
 ExternalProject_Add(crossc
-    DEPENDS
-        spirv-cross
     GIT_REPOSITORY https://github.com/rossy/crossc.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
@@ -15,19 +13,11 @@ ExternalProject_Add(crossc
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-ExternalProject_Add_Step(crossc remove-submodule
-    DEPENDEES download update
-    WORKING_DIRECTORY <SOURCE_DIR>
-    COMMAND chmod 755 SPIRV-Cross
-    COMMAND ${CMAKE_COMMAND} -E remove_directory SPIRV-Cross
-    LOG 1
-)
-
-ExternalProject_Add_Step(crossc symlink-submodule
-    DEPENDEES remove-submodule
+ExternalProject_Add_Step(crossc update-submodule
+    DEPENDEES download update force-update
     DEPENDERS build
     WORKING_DIRECTORY <SOURCE_DIR>
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_CURRENT_BINARY_DIR}/spirv-cross-prefix/src/spirv-cross SPIRV-Cross
+    COMMAND git submodule update --remote --recursive
     LOG 1
 )
 
