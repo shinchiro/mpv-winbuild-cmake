558c4c8f2cf3ab506dcc54a50d1cf2effd445183
shinchiro <shinchiro@users.noreply.github.com>
2019-03-10 09:18:08 +0800

    mujs: revert to commit 8e7bd38
    Some bullshit happened in the repo and this is only a way to make it install successfully.

--- packages/mujs.cmake
+++ packages/mujs.cmake
@@ -1,23 +1,26 @@
+set(flag
+"CFLAGS='-UHAVE_READLINE' LIBREADLINE=''
+CC=${TARGET_ARCH}-gcc
+AR=${TARGET_ARCH}-ar
+RANLIB=${TARGET_ARCH}-ranlib
+prefix=${MINGW_INSTALL_PREFIX}")
+
 ExternalProject_Add(mujs
     GIT_REPOSITORY git://git.ghostscript.com/mujs.git
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${MAKE}
-        CFLAGS='-UHAVE_READLINE' LIBREADLINE='' # undefine readline as we dont want readline
-        CC=${TARGET_ARCH}-gcc
-        AR=${TARGET_ARCH}-ar
-        RANLIB=${TARGET_ARCH}-ranlib
-        prefix=${MINGW_INSTALL_PREFIX}
-        build/release/libmujs.a
-        build/release/mujs.pc
-    INSTALL_COMMAND install -d ${MINGW_INSTALL_PREFIX}/include
-            COMMAND install -d ${MINGW_INSTALL_PREFIX}/lib
-            COMMAND install -d ${MINGW_INSTALL_PREFIX}/lib/pkgconfig
-            COMMAND install -m644 mujs.h  ${MINGW_INSTALL_PREFIX}/include
-            COMMAND install -m644 build/release/libmujs.a ${MINGW_INSTALL_PREFIX}/lib
-            COMMAND install -m644 build/release/mujs.pc ${MINGW_INSTALL_PREFIX}/lib/pkgconfig
+    BUILD_COMMAND ${MAKE} ${flag}
+    INSTALL_COMMAND ${MAKE} ${flag} install-static
     BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+ExternalProject_Add_Step(mujs rename-executablesuffix
+    DEPENDEES build
+    DEPENDERS install
+    WORKING_DIRECTORY <SOURCE_DIR>/build/release
+    COMMAND mv mujs.exe mujs
+    LOG 1
 )
 
 force_rebuild_git(mujs)
