5aeb0d4f859db5edd02c980c126fbd04ae0c0d6a
NotTsunami <ireallylikeshrek@gmail.com>
2019-08-07 12:46:55 -0400

    ogg, speex: Update to new location
    Xiph has moved their location to an FTP server, we already updated vorbis but not these repos

--- packages/ogg.cmake
+++ packages/ogg.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add(ogg
-    URL http://downloads.xiph.org/releases/ogg/libogg-1.3.3.tar.xz
+    URL https://ftp.osuosl.org/pub/xiph/releases/ogg/libogg-1.3.3.tar.xz
     URL_HASH SHA256=4f3fc6178a533d392064f14776b23c397ed4b9f48f5de297aba73b643f955c08
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
--- packages/speex.cmake
+++ packages/speex.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(speex
     DEPENDS ogg
-    URL "http://downloads.xiph.org/releases/speex/speex-1.2.0.tar.gz"
+    URL "https://ftp.osuosl.org/pub/xiph/releases/speex/speex-1.2.0.tar.gz"
     URL_HASH SHA256=eaae8af0ac742dc7d542c9439ac72f1f385ce838392dc849cae4536af9210094
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC}
