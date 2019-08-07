72cbe817619c175b82940c963eb26fed90c2706e
NotTsunami <ireallylikeshrek@gmail.com>
2019-08-07 12:47:21 -0400

    freetype2: Use https

--- packages/freetype2.cmake
+++ packages/freetype2.cmake
@@ -3,7 +3,7 @@ configure_file(${CMAKE_CURRENT_SOURCE_DIR}/ft2exec.in ${CMAKE_CURRENT_BINARY_DIR
 ExternalProject_Add(freetype2
     DEPENDS libpng zlib
     #GIT_REPOSITORY git://git.sv.gnu.org/freetype/freetype2.git
-    GIT_REPOSITORY http://git.sv.nongnu.org/r/freetype/freetype2.git
+    GIT_REPOSITORY https://git.sv.nongnu.org/r/freetype/freetype2.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/ft2exec <SOURCE_DIR>/configure
