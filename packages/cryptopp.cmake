a194a410877f9a4637dd68fa2454f7122e19d14f
NotTsunami <ireallylikeshrek@gmail.com>
2019-08-07 12:57:44 -0400

    cryptopp, expat: Update minor versions
    cryptopp 8.1.0->cryptopp 8.2.0
    expat 2.2.6->expat 2.2.7

--- packages/cryptopp.cmake
+++ packages/cryptopp.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(cryptopp
-    URL https://github.com/weidai11/cryptopp/archive/CRYPTOPP_8_1_0.tar.gz
-    URL_HASH SHA256=8a4e4773a39b0c07d7cea1b8be7a3f7a9d126bd3ac9a9f072f82d3a53a474a87
+    URL https://github.com/weidai11/cryptopp/archive/CRYPTOPP_8_2_0.tar.gz
+    URL_HASH SHA256=e3bcd48a62739ad179ad8064b523346abb53767bcbefc01fe37303412292343e
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${MAKE}
         CXX=${TARGET_ARCH}-g++
--- packages/expat.cmake
+++ packages/expat.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(expat
-    URL "https://download.sourceforge.net/expat/expat-2.2.6.tar.bz2"
-    URL_HASH SHA256=17b43c2716d521369f82fc2dc70f359860e90fa440bea65b3b85f0b246ea81f2
+    URL "https://download.sourceforge.net/expat/expat-2.2.7.tar.bz2"
+    URL_HASH SHA256=cbc9102f4a31a8dafd42d642e9a3aa31e79a0aedaa1f6efd2795ebc83174ec18
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
