c9e1e44d9d8bd90bc3b46c9414166c8763f9928c
shinchiro <shinchiro@users.noreply.github.com>
2019-10-02 18:31:41 +0800

    opus: switch to cmake build

--- packages/opus.cmake
+++ packages/opus.cmake
@@ -1,16 +1,16 @@
 ExternalProject_Add(opus
     GIT_REPOSITORY https://git.xiph.org/opus.git
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/autogen.sh
-    COMMAND ${EXEC} <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-        --disable-doc
-        --disable-extra-programs
-        --disable-stack-protector
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+    CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+        -G Ninja
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DOPUS_STACK_PROTECTOR=OFF
+        -DOPUS_BUILD_PROGRAMS=OFF
+        -DBUILD_TESTING=OFF
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
