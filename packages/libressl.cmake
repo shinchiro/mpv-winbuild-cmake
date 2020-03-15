ffc0ed7b00d54a3aad6b6f19400066d8713a2827
shinchiro <shinchiro@users.noreply.github.com>
2020-03-15 21:36:25 +0800

    libressl: fix bug in static libs name
    kind of bullshit.

--- packages/libressl.cmake
+++ packages/libressl.cmake
@@ -2,6 +2,7 @@ ExternalProject_Add(libressl
     GIT_REPOSITORY https://github.com/libressl-portable/portable.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/libressl-*.patch
     CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
@@ -9,6 +10,7 @@ ExternalProject_Add(libressl
         -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
         -DLIBRESSL_APPS=OFF
         -DLIBRESSL_TESTS=OFF
+        -DBUILD_SHARED_LIBS=OFF
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
