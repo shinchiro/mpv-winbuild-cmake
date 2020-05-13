e8ebbcedd1bc5817cdd80a8556876d7bc1a42765
shinchiro <shinchiro@users.noreply.github.com>
2020-05-14 06:34:28 +0800

    luajit: adjust build params for patch

--- packages/luajit.cmake
+++ packages/luajit.cmake
@@ -21,13 +21,15 @@ ExternalProject_Add(luajit
         libiconv
     GIT_REPOSITORY https://github.com/LuaJIT/LuaJIT.git
     GIT_SHALLOW 1
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/luajit-*.patch
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${MAKE}
         "HOST_CC='${LUAJIT_HOST_GCC} ${LUAJIT_GCC_ARGS}'"
         CROSS=${TARGET_ARCH}-
         TARGET_SYS=Windows
-        BUILDMODE=static amalg
+        BUILDMODE=static
+        CFLAGS='-D_WIN32_WINNT=0x0602 -DUNICODE'
         XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT ${ENABLE_GC64}'
     INSTALL_COMMAND ${MAKE}
         "HOST_CC='${LUAJIT_HOST_GCC} ${LUAJIT_GCC_ARGS}'"
@@ -35,6 +37,7 @@ ExternalProject_Add(luajit
         TARGET_SYS=Windows
         BUILDMODE=static
         FILE_T=luajit.exe
+        CFLAGS='-D_WIN32_WINNT=0x0602 -DUNICODE'
         XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT ${ENABLE_GC64}'
         PREFIX=${MINGW_INSTALL_PREFIX}
         install
