eaf25b681dfa9306042395a85ffb6b8f6bb67db5
shinchiro <shinchiro@users.noreply.github.com>
2020-05-31 03:44:35 +0800

    mujs: remove install hack

--- packages/mujs.cmake
+++ packages/mujs.cmake
@@ -9,18 +9,15 @@ ExternalProject_Add(mujs
     GIT_REPOSITORY https://github.com/ccxvii/mujs.git
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${MAKE} ${flag}
-    INSTALL_COMMAND ${MAKE} ${flag} install-static
+    BUILD_COMMAND ${MAKE} ${flag} build/release/libmujs.a build/release/mujs.pc
+    INSTALL_COMMAND install -d ${MINGW_INSTALL_PREFIX}/include
+            COMMAND install -d ${MINGW_INSTALL_PREFIX}/lib
+            COMMAND install -d ${MINGW_INSTALL_PREFIX}/lib/pkgconfig
+            COMMAND install -m 644 mujs.h                  ${MINGW_INSTALL_PREFIX}/include
+            COMMAND install -m 644 build/release/libmujs.a ${MINGW_INSTALL_PREFIX}/lib
+            COMMAND install -m 644 build/release/mujs.pc   ${MINGW_INSTALL_PREFIX}/lib/pkgconfig
     BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-)
-
-ExternalProject_Add_Step(mujs rename-executablesuffix
-    DEPENDEES build
-    DEPENDERS install
-    WORKING_DIRECTORY <SOURCE_DIR>/build/release
-    COMMAND mv mujs.exe mujs
-    LOG 1
 )
 
 force_rebuild_git(mujs)
