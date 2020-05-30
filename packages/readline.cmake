7f775427d13d809846311e4b66cf4fe71305dd00
shinchiro <shinchiro@users.noreply.github.com>
2020-05-31 02:05:14 +0800

    readline: switch to git
    also fix things after upgrade to gcc 10.

--- packages/readline.cmake
+++ packages/readline.cmake
@@ -1,13 +1,16 @@
 ExternalProject_Add(readline
     DEPENDS
         termcap
-    URL https://ftp.gnu.org/gnu/readline/readline-8.0.tar.gz
-    URL_HASH SHA256=e339f51971478d369f8a053a330a190781acb9864cf4c541060f12078948e461
+    GIT_REPOSITORY https://git.sailfishos.org/mirror/readline.git
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --enable-static
         --disable-shared
+        --without-curses
+        CFLAGS='-DNEED_EXTERN_PC -DS_IFLNK=0120000'
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
