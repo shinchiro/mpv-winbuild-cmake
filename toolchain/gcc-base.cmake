be4d3172292adf1ba7c371de07710a744a55e6ce
shinchiro <shinchiro@users.noreply.github.com>
2020-06-07 21:38:25 +0800

    gcc: update to 10.1.1+20200606

--- toolchain/gcc-base.cmake
+++ toolchain/gcc-base.cmake
@@ -13,8 +13,8 @@ ExternalProject_Add(gcc-base
     STAMP_DIR gcc-prefix/src/gcc-stamp
     SOURCE_DIR gcc-prefix/src/gcc
     BINARY_DIR gcc-prefix/src/gcc-build
-    URL ftp://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/10-20200523/gcc-10-20200523.tar.xz
-    URL_HASH SHA512=2b1d57056d154a2f7453d16217dfc232d29866c5bcfbee8fafdd871f2ae7f14bc2c99b9d2df89bcb407833510b0a3dfe778df9f4d3a68540da6863157f524f6d
+    URL ftp://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/10-20200606/gcc-10-20200606.tar.xz
+    URL_HASH SHA512=33b6cbba401430750e027e12fd3964fe8a9eef7fea0344c0680f368c94fb9f13e67061850a059bb777b8c12bac9adbef37c731ca698e4a7b41ae9f0ce316ebaa
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
         --target=${TARGET_ARCH}
         --prefix=${CMAKE_INSTALL_PREFIX}
