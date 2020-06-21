6f6d1cccf9056aacdfbcd06be5295a599d9f6fef
shinchiro <shinchiro@users.noreply.github.com>
2020-06-21 23:02:00 +0800

    fribidi: fix building static lib

--- packages/fribidi.cmake
+++ packages/fribidi.cmake
@@ -1,6 +1,7 @@
 ExternalProject_Add(fribidi
     GIT_REPOSITORY https://github.com/fribidi/fribidi.git
     GIT_SHALLOW 1
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/fribidi-*.patch
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} meson <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
@@ -8,8 +9,10 @@ ExternalProject_Add(fribidi
         --cross-file=${MESON_CROSS}
         --buildtype=release
         --default-library=static
-        -Ddeprecated=false
+        -Dcpp_args='-DFRIBIDI_LIB_STATIC'
         -Ddocs=false
+        -Dbin=false
+        -Dtests=false
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
