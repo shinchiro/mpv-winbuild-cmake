6c4515c083fd8010e29197100e91252ebc2fc97f
shinchiro <shinchiro@users.noreply.github.com>
2020-08-08 08:29:25 +0800

    harfbuzz: use meson to build

--- packages/harfbuzz.cmake
+++ packages/harfbuzz.cmake
@@ -1,27 +1,27 @@
-find_program(RAGEL ragel)
-
-if(NOT RAGEL)
-    message(FATAL_ERROR "Ragel is required to build Harfbuzz")
-endif()
-
 ExternalProject_Add(harfbuzz
-    DEPENDS freetype2 libpng
-    GIT_REPOSITORY https://github.com/behdad/harfbuzz.git
+    DEPENDS
+        freetype2
+        libpng
+    GIT_REPOSITORY https://github.com/harfbuzz/harfbuzz.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
+    CONFIGURE_COMMAND ${EXEC} meson <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-        --with-icu=no
-        --with-glib=no
-        --with-gobject=no
-        --disable-gtk-doc-html
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+        --libdir=${MINGW_INSTALL_PREFIX}/lib
+        --cross-file=${MESON_CROSS}
+        --buildtype=release
+        --default-library=static
+        -Dicu=disabled
+        -Dglib=disabled
+        -Dgobject=disabled
+        -Dtests=disabled
+        -Ddocs=disabled
+        -Dbenchmark=disabled
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(harfbuzz)
+force_meson_configure(harfbuzz)
 extra_step(harfbuzz)
-autogen(harfbuzz)
