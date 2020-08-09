1d10c814f2ed11a488900be29cdd0d462642db9c
myfreeer <myfreeer@users.noreply.github.com>
2020-08-09 15:57:20 +0800

    binutils: upgrade to v2.35

--- toolchain/binutils.cmake
+++ toolchain/binutils.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(binutils
-    URL https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz
-    URL_HASH SHA512=2c7976939dcf5e8c5b7374cccd39bfe803b1bec73c6abfa0eb17c24e1942574c6bdb874c66a092a82adc443182eacd8a5a8001c19a76101f0c7ba40c27de0bbd
+    URL https://ftp.gnu.org/gnu/binutils/binutils-2.35.tar.xz
+    URL_HASH SHA512=9f222e4ab6720036402d03904fb11b73ab87714b85cd84997f7d357f405c7e10581d70202f9165a1ee0c70538632db27ecc9dfe627dddb1e6bc7edb1537cf786
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
         --target=${TARGET_ARCH}
         --prefix=${CMAKE_INSTALL_PREFIX}
