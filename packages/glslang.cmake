74a9c334b6ecef519a26351ebd3ae10d6ca1c267
shinchiro <shinchiro@users.noreply.github.com>
2020-08-23 09:31:56 +0800

    glslang: remove patch

--- packages/glslang.cmake
+++ packages/glslang.cmake
@@ -1,7 +1,6 @@
 ExternalProject_Add(glslang
     GIT_REPOSITORY https://github.com/KhronosGroup/glslang.git
     GIT_SHALLOW 1
-    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/glslang-*.patch
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
