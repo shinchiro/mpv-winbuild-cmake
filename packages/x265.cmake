f98b97f4473b84bad0e5d37bf8850f4577c831a9
shinchiro <shinchiro@users.noreply.github.com>
2020-08-30 11:10:05 +0800

    x265: disable git shallow
    due to recent commit. Fix #90, probably.

--- packages/x265.cmake
+++ packages/x265.cmake
@@ -7,7 +7,6 @@ endif()
 
 ExternalProject_Add(x265-base
     GIT_REPOSITORY https://bitbucket.org/multicoreware/x265_git.git
-    GIT_SHALLOW 1
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
