0371bae3febb8f2c268c060912608ba72c62ff66
shinchiro <shinchiro@users.noreply.github.com>
2020-11-22 20:23:01 +0800

    autobuild.sh: update method to check remote's git hash

--- scripts/autobuild.sh
+++ scripts/autobuild.sh
@@ -66,7 +66,7 @@ download_mpv_package() {
     local package_url="https://codeload.github.com/shinchiro/mpv-packaging/zip/master"
     if [ -e mpv-packaging.zip ]; then
         echo "Package exists. Check if it is newer.."
-        remote_commit=$(curl -sI $package_url | grep -Po 'ETag: "\K[^"]+')
+        remote_commit=$(git ls-remote https://github.com/shinchiro/mpv-packaging.git master | awk '{print $1;}')
         local_commit=$(unzip -z mpv-packaging.zip | tail +2)
         if [ "$remote_commit" != "$local_commit" ]; then
             wget -O mpv-packaging.zip $package_url
