cf52d9feef1cddfe0b207ad0f21a5225237d10a4
shinchiro <shinchiro@users.noreply.github.com>
2020-12-19 19:38:55 +0800

    angle-headers: switch to my gitlab's mirror
    gitlab allow us to download specific folder without hassle. Fix #102

--- packages/angle-headers.cmake
+++ packages/angle-headers.cmake
@@ -1,10 +1,10 @@
 ExternalProject_Add(angle-headers
-    SVN_REPOSITORY https://github.com/google/angle/trunk/include
+    URL https://gitlab.com/shinchiro/angle/-/archive/master/angle-master.zip?path=include
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/EGL ${MINGW_INSTALL_PREFIX}/include/EGL
-            COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/KHR ${MINGW_INSTALL_PREFIX}/include/KHR
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include/EGL ${MINGW_INSTALL_PREFIX}/include/EGL
+            COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include/KHR ${MINGW_INSTALL_PREFIX}/include/KHR
     LOG_DOWNLOAD 1
 )
 
