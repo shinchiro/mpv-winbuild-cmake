211f1c828762d54ac0e189c88296608c2fc57d34
hooke007 <41094733+hooke007@users.noreply.github.com>
2021-07-06 21:33:03 +0800

    ogg: update to 1.3.5

--- packages/ogg.cmake
+++ packages/ogg.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(ogg
-    URL https://ftp.osuosl.org/pub/xiph/releases/ogg/libogg-1.3.4.tar.xz
-    URL_HASH SHA256=c163bc12bc300c401b6aa35907ac682671ea376f13ae0969a220f7ddf71893fe
+    URL https://ftp.osuosl.org/pub/xiph/releases/ogg/libogg-1.3.5.tar.xz
+    URL_HASH SHA256=c4d91be36fc8e54deae7575241e03f4211eb102afb3fc0775fbbc1b740016705
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
