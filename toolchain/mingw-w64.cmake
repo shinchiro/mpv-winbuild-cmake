f084f722eebe60f747d79bdb1553b0c41da0e41d
shinchiro <shinchiro@users.noreply.github.com>
2021-07-25 23:09:00 +0800

    mingw: back to master
    Master branch required latest autoconf & automake which not present on some linux distribution.

--- toolchain/mingw-w64.cmake
+++ toolchain/mingw-w64.cmake
@@ -1,7 +1,6 @@
 ExternalProject_Add(mingw-w64
     PREFIX mingw-w64-prefix
     GIT_REPOSITORY https://github.com/mirror/mingw-w64.git
-    GIT_TAG f3855e2caa576b1a6288129f8f99a56d2ef969dd
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
