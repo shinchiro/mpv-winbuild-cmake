97ab461af6a200a2cb2ab553f5d42d5773c0c2b4
shinchiro <shinchiro@users.noreply.github.com>
2021-09-12 10:02:27 +0800

    libsrt: add new package
    Fix #147

---
+++ packages/libsrt.cmake
@@ -0,0 +1,21 @@
+ExternalProject_Add(libsrt
+    GIT_REPOSITORY https://github.com/Haivision/srt.git
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/libsrt-*.patch
+    CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+        -G Ninja
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DENABLE_STDCXX_SYNC=ON
+        -DENABLE_APPS=OFF
+        -DENABLE_SHARED=OFF
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
+    BUILD_IN_SOURCE 1
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+force_rebuild_git(libsrt)
+extra_step(libsrt)
