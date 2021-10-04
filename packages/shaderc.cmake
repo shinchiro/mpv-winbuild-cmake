2f4abf93c0b8207f732c00df17d53ce14bf80701
shinchiro <shinchiro@users.noreply.github.com>
2021-10-04 12:50:24 +0800

    shaderc: install .pc file

--- packages/shaderc.cmake
+++ packages/shaderc.cmake
@@ -9,9 +9,12 @@ ExternalProject_Add(shaderc
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
         -DCMAKE_TOOLCHAIN_FILE=<SOURCE_DIR>/cmake/linux-mingw-toolchain.cmake
         -DSHADERC_SKIP_TESTS=ON
         -DSHADERC_SKIP_SPVC=ON
+        -DSHADERC_SKIP_INSTALL=ON
+        -DSHADERC_SKIP_EXAMPLES=ON
         -DMINGW_COMPILER_PREFIX=${TARGET_ARCH}
         -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -std=c++11'
     BUILD_COMMAND ${MAKE} -C <BINARY_DIR>
@@ -32,8 +35,8 @@ ExternalProject_Add_Step(shaderc symlink
 ExternalProject_Add_Step(shaderc manual-install
     DEPENDEES build
     COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/libshaderc/include/shaderc ${MINGW_INSTALL_PREFIX}/include/shaderc
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/libshaderc_util/include/libshaderc_util ${MINGW_INSTALL_PREFIX}/include/libshaderc_util
     COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/libshaderc/libshaderc_combined.a ${MINGW_INSTALL_PREFIX}/lib/libshaderc_combined.a
+    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/shaderc_combined.pc ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/shaderc_combined.pc
     COMMENT "Manually installing"
 )
 
