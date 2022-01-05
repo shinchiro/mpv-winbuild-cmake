1fde340f4f8fe663ae18027a796f54006009dcec
shinchiro <shinchiro@users.noreply.github.com>
2022-01-05 11:32:02 +0800

    fontconfig,uchardet: switch git's url
    Switch to github for more stability.

--- packages/uchardet.cmake
+++ packages/uchardet.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add(uchardet
-    GIT_REPOSITORY https://gitlab.freedesktop.org/uchardet/uchardet.git
+    GIT_REPOSITORY https://github.com/freedesktop/uchardet.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
     CMAKE_ARGS
