9c6607fd7ae9d7c40b956b5293da9184976ed562
shinchiro <shinchiro@users.noreply.github.com>
2022-02-15 09:05:21 +0800

    avisynth-headers: install header properly
    Fix #181

--- packages/avisynth-headers.cmake
+++ packages/avisynth-headers.cmake
@@ -2,12 +2,12 @@ ExternalProject_Add(avisynth-headers
     GIT_REPOSITORY https://github.com/AviSynth/AviSynthPlus.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
-    CMAKE_ARGS
+    CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -DCMAKE_BUILD_TYPE=Release
         -DHEADERS_ONLY=ON
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
     BUILD_COMMAND ""
-    INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
+    INSTALL_COMMAND ${MAKE} VersionGen install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_INSTALL 1
 )
 force_rebuild_git(avisynth-headers)
