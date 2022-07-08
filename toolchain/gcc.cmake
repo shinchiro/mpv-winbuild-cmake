d8e1181d9abc810e204942bae7cff7d7aaf60f5b
shinchiro <shinchiro@users.noreply.github.com>
2022-07-08 16:22:18 +0800

    gcc: update to 12.1.1+20220702

--- toolchain/gcc.cmake
+++ toolchain/gcc.cmake
@@ -9,8 +9,8 @@ endif()
 ExternalProject_Add(gcc
     DEPENDS
         mingw-w64-headers
-    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-12.1.0/gcc-12.1.0.tar.xz
-    URL_HASH SHA512=2121d295292814a6761edf1fba08c5f633ebe16f52b80e7b73a91050e71e1d2ed98bf17eebad263e191879561c02b48906c53faa4c4670c486a26fc75df23900
+    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/12-20220702/gcc-12-20220702.tar.xz
+    URL_HASH SHA512=e5ab44e79f641ace6d9d77ffb176f3309d35e8f36b6bdd42f09e29fcc82766933e0fb8f86be84bf7419fe9918c554544d34c448b706ec0cb052e738e5594683c
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
         --target=${TARGET_ARCH}
