7e2e20c30573d3bb1a82aef7e3d97a9ac821123c
shinchiro <shinchiro@users.noreply.github.com>
2022-07-17 06:33:05 +0800

    rubberband: change to original repo

--- packages/rubberband.cmake
+++ packages/rubberband.cmake
@@ -1,14 +1,23 @@
 ExternalProject_Add(rubberband
-    GIT_REPOSITORY https://github.com/lachs0r/rubberband.git
+    GIT_REPOSITORY https://github.com/breakfastquay/rubberband.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_SHALLOW 1
+    GIT_REMOTE_NAME origin
+    GIT_TAG default
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${MAKE} CC=${TARGET_ARCH}-gcc CXX=${TARGET_ARCH}-g++ AR=${TARGET_ARCH}-ar static
-    INSTALL_COMMAND ${MAKE} PREFIX=${MINGW_INSTALL_PREFIX} install-static
-    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ${EXEC} meson <BINARY_DIR> <SOURCE_DIR>
+        --prefix=${MINGW_INSTALL_PREFIX}
+        --libdir=${MINGW_INSTALL_PREFIX}/lib
+        --cross-file=${MESON_CROSS}
+        --buildtype=release
+        --default-library=static
+        -Dfft=builtin
+        -Dresampler=builtin
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(rubberband)
+force_meson_configure(rubberband)
 cleanup(rubberband install)
