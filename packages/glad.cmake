4828f4641ec807a977a8242c06ffdba81be43011
shinchiro <shinchiro@users.noreply.github.com>
2022-07-31 11:37:22 +0800

    libplacebo: add glad dependency

---
+++ packages/glad.cmake
@@ -0,0 +1,15 @@
+ExternalProject_Add(glad
+    GIT_REPOSITORY https://github.com/Dav1dde/glad.git
+    SOURCE_DIR ${SOURCE_LOCATION}
+    GIT_SHALLOW 1
+    GIT_REMOTE_NAME origin
+    GIT_TAG glad2
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD 1 LOG_UPDATE 1
+)
+
+force_rebuild_git(glad)
+cleanup(glad install)
--- packages/libplacebo.cmake
+++ packages/libplacebo.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(libplacebo
         vulkan
         shaderc
         lcms2
-        libepoxy
+        glad
     GIT_REPOSITORY https://github.com/haasn/libplacebo.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_SHALLOW 1
@@ -20,6 +20,17 @@ ExternalProject_Add(libplacebo
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+get_property(src_glad TARGET glad PROPERTY _EP_SOURCE_DIR)
+
+ExternalProject_Add_Step(libplacebo symlink
+    DEPENDEES download update patch
+    DEPENDERS configure
+    WORKING_DIRECTORY <SOURCE_DIR>/3rdparty
+    COMMAND rm -rf glad
+    COMMAND ${CMAKE_COMMAND} -E create_symlink ${src_glad} glad
+    COMMENT "Symlinking glad"
 )
 
 force_rebuild_git(libplacebo)
