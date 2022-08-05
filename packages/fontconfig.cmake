a3d89553e0c21cd0a168b914036b941908e574a5
shinchiro <shinchiro@users.noreply.github.com>
2022-08-06 04:46:02 +0800

    fontconfig: migrate to meson

--- packages/fontconfig.cmake
+++ packages/fontconfig.cmake
@@ -11,17 +11,21 @@ ExternalProject_Add(fontconfig
     GIT_TAG main
     GIT_SHALLOW 1
     PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/fontconfig-*.patch
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
+    CONFIGURE_COMMAND ${EXEC} meson <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
-        --enable-iconv
-        --disable-shared
-        --disable-docs
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+        --libdir=${MINGW_INSTALL_PREFIX}/lib
+        --cross-file=${MESON_CROSS}
+        --buildtype=release
+        --default-library=static
+        -Ddoc=disabled
+        -Dtests=disabled
+        -Dtools=disabled
+        -Dcache-build=disabled
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(fontconfig)
-autogen(fontconfig)
+force_meson_configure(fontconfig)
 cleanup(fontconfig install)
