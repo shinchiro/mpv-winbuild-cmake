8abacaee5f6f1f41a0ea368f3c14198ed5710023
zhongfly <11155705+zhongfly@users.noreply.github.com>
2022-09-23 21:06:05 +0800

    expat: update to 2.4.9

--- packages/expat.cmake
+++ packages/expat.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(expat
-    URL https://github.com/libexpat/libexpat/releases/download/R_2_4_8/expat-2.4.8.tar.xz
-    URL_HASH SHA256=f79b8f904b749e3e0d20afeadecf8249c55b2e32d4ebb089ae378df479dcaf25
+    URL https://github.com/libexpat/libexpat/releases/download/R_2_4_9/expat-2.4.9.tar.xz
+    URL_HASH SHA256=6e8c0728fe5c7cd3f93a6acce43046c5e4736c7b4b68e032e9350daa0efc0354
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
