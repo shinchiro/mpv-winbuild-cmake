30da1c5444320e6950ab424d82adab697e57cee4
shinchiro <shinchiro@users.noreply.github.com>
2023-01-13 12:56:59 +0800

    spirv-*: switch to main branch
    master branch was deleted.

--- packages/spirv-tools.cmake
+++ packages/spirv-tools.cmake
@@ -2,6 +2,8 @@ ExternalProject_Add(spirv-tools
     GIT_REPOSITORY https://github.com/KhronosGroup/SPIRV-Tools.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_SHALLOW 1
+    GIT_REMOTE_NAME origin
+    GIT_TAG main
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
