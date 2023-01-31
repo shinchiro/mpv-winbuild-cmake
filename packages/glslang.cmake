a9e1712af0eb3cc1d5e926e0ea11d41ec6131ad0
shinchiro <shinchiro@users.noreply.github.com>
2023-01-31 21:25:06 +0800

    glslang: change to main branch

--- packages/glslang.cmake
+++ packages/glslang.cmake
@@ -2,6 +2,8 @@ ExternalProject_Add(glslang
     GIT_REPOSITORY https://github.com/KhronosGroup/glslang.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_REMOTE_NAME origin
+    GIT_TAG main
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
