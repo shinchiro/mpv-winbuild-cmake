334ab2f883b16f168fc7371080d5bf4255a1dc64
shinchiro <shinchiro@users.noreply.github.com>
2023-02-22 16:02:32 +0800

    toolchain: install rustup in separate folder
    It will install both i686 & x86_64 targets which can be reused

--- toolchain/rustup.cmake
+++ toolchain/rustup.cmake
@@ -1,16 +1,10 @@
-if(${TARGET_CPU} MATCHES "x86_64")
-    set(target "x86_64-pc-windows-gnu")
-else()
-    set(target "i686-pc-windows-gnu")
-endif()
-
 ExternalProject_Add(rustup
     DOWNLOAD_COMMAND ""
     UPDATE_COMMAND ""
     SOURCE_DIR rustup-prefix/src
     CONFIGURE_COMMAND ${EXEC}
         curl -sSf https://sh.rustup.rs |
-        sh -s -- -y --default-toolchain stable --target ${target} --no-modify-path --profile minimal
+        sh -s -- -y --default-toolchain stable --target x86_64-pc-windows-gnu,i686-pc-windows-gnu --no-modify-path --profile minimal
     BUILD_COMMAND ${EXEC} rustup update
     INSTALL_COMMAND ${EXEC} cargo install cargo-c --profile=release-strip --features=vendored-openssl
     LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
