8c37aba0103c4c1f344fa685e8abd020a4a799d6
shinchiro <shinchiro@users.noreply.github.com>
2023-02-23 22:53:44 +0800

    libdovi: improve thing
    * sparse checkout dolby_vision directory only
    * set target dir to <BINARY_DIR>

--- packages/libdovi.cmake
+++ packages/libdovi.cmake
@@ -1,15 +1,17 @@
 ExternalProject_Add(libdovi
     GIT_REPOSITORY https://github.com/quietvoid/dovi_tool.git
     SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_CLONE_FLAGS "--sparse --filter=tree:0"
+    GIT_CLONE_POST_COMMAND "sparse-checkout set dolby_vision"
     GIT_REMOTE_NAME origin
     GIT_TAG main
     UPDATE_COMMAND ""
     PATCH_COMMAND ""
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${EXEC} cargo
-        --config profile.release.codegen-units=1
-        cinstall
+    BUILD_COMMAND ${EXEC}
+        CARGO_BUILD_TARGET_DIR=<BINARY_DIR>
+        CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1
+        cargo cinstall
         --manifest-path <SOURCE_DIR>/dolby_vision/Cargo.toml
         --prefix ${MINGW_INSTALL_PREFIX}
         --target ${TARGET_CPU}-pc-windows-gnu
