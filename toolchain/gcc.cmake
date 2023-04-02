089eb38857f96feb7ea702050b42a89803804dac
shinchiro <shinchiro@users.noreply.github.com>
2023-04-02 17:11:23 +0800

    gcc: update to 12.2.1+20230401

--- toolchain/gcc.cmake
+++ toolchain/gcc.cmake
@@ -7,9 +7,9 @@ endif()
 ExternalProject_Add(gcc
     DEPENDS
         mingw-w64-headers
-    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/12-20230218/gcc-12-20230218.tar.xz
+    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/12-20230401/gcc-12-20230401.tar.xz
     # https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/12-20221217/sha512.sum
-    URL_HASH SHA512=bf2b109a12a981ae97ca4c6d9a27b11adcf1c7acaf167c532936a027dad4217e75b197fd12b2c5e37f6e89fe39a54f483984023accfb76f7517cc3d35ced416a
+    URL_HASH SHA512=b0757f56cd778ca537090c79d1ca606e28d258473a9add59df9be39dcfe3047cbe324e81a389f101d8ded31419b0e535410997593bd99a6fa22730288362de4e
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=54412
     PATCH_COMMAND ${EXEC} curl -sL https://salsa.debian.org/mingw-w64-team/gcc-mingw-w64/-/raw/5e7d749d80e47d08e34a17971479d06cd423611e/debian/patches/vmov-alignment.patch | patch -p2
