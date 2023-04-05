8572d51c08620641a97c7b6f73012665a4bdd1fe
zhongfly <11155705+zhongfly@users.noreply.github.com>
2023-04-05 11:33:20 +0800

    libopenmpt: update to 0.6.9

--- packages/libopenmpt.cmake
+++ packages/libopenmpt.cmake
@@ -4,8 +4,8 @@ ExternalProject_Add(libopenmpt
         ogg
         vorbis
         libsdl2
-    URL https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-0.6.7+release.autotools.tar.gz
-    URL_HASH SHA256=2174AC0F5A148BA684DB768A47EDF783EFF9084FBCA5FEF6C997501643100163
+    URL https://lib.openmpt.org/files/libopenmpt/src/libopenmpt-0.6.9+release.autotools.tar.gz
+    URL_HASH SHA256=479E975ABB7DC0FA9CAD41BDD31F255D78D43E0726546208058D3C3FCF7B6E5A
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
