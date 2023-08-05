7e3e44d2d0037ee1e2217ca7f9e9745eb6692c21
trclst <141440559+trclst@users.noreply.github.com>
2023-08-05 12:35:11 +0200

    gmp: update to 6.3.0

--- packages/gmp.cmake
+++ packages/gmp.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(gmp
-    URL https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.xz
-    URL_HASH SHA256=fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2
+    URL https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz
+    URL_HASH SHA256=64b1102fa22093515b02ef33dd8739dee1ba57e9dbba6a092942b8bbed1a1c5e
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         CC_FOR_BUILD=cc
