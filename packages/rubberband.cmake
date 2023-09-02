40c7e7622ad1252eaa6f0cfe2adef50020ec140d
ghost <>
2023-09-02 08:25:00 +0000

    rubberband: disable jni

--- packages/rubberband.cmake
+++ packages/rubberband.cmake
@@ -13,6 +13,7 @@ ExternalProject_Add(rubberband
         --default-library=static
         -Dfft=builtin
         -Dresampler=builtin
+        -Djni=disabled
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
