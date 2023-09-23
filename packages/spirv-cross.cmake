dd5c4e0abca5e6c0cc0cd5615eafe470db4f6f01
Andarwinux <144242044+Andarwinux@users.noreply.github.com>
2023-09-23 17:08:34 +0000

    spirv-cross: remove patch

--- packages/spirv-cross.cmake
+++ packages/spirv-cross.cmake
@@ -5,13 +5,12 @@ ExternalProject_Add(spirv-cross
     GIT_REMOTE_NAME origin
     GIT_TAG main
     UPDATE_COMMAND ""
-    PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/spirv-cross-*.patch
     CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
         -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
-        -DSPIRV_CROSS_SHARED=ON
+        -DSPIRV_CROSS_STATIC=ON
         -DSPIRV_CROSS_CLI=OFF
         -DSPIRV_CROSS_ENABLE_TESTS=OFF
         -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -D__USE_MINGW_ANSI_STDIO'
@@ -22,8 +21,8 @@ ExternalProject_Add(spirv-cross
 
 ExternalProject_Add_Step(spirv-cross symlink
     DEPENDEES install
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/spirv-cross-c-shared.pc
-                                               ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/spirv-cross.pc
+    COMMAND ${CMAKE_COMMAND} -E create_symlink ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/spirv-cross-c.pc
+                                               ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/spirv-cross-c-shared.pc
 )
 
 force_rebuild_git(spirv-cross)
