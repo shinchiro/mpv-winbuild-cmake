c008fb5b34bc83f76c5b4f8af0a626334293c536
Andarwinux <Andarwinux@users.noreply.github.com>
2023-10-04 14:55:59 +0000

    dav1d: enable xxhash

--- packages/dav1d.cmake
+++ packages/dav1d.cmake
@@ -1,4 +1,6 @@
 ExternalProject_Add(dav1d
+    DEPENDS
+        xxhash
     GIT_REPOSITORY https://code.videolan.org/videolan/dav1d.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
@@ -11,6 +13,7 @@ ExternalProject_Add(dav1d
         --default-library=static
         -Denable_tools=false
         -Denable_tests=false
+        -Dxxhash_muxer=enabled
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
