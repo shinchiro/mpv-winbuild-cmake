d6ab23783f4a8975f53e451cc46837d25254ef74
Andarwinux <Andarwinux@users.noreply.github.com>
2023-10-07 16:45:39 +0000

    mbedtls: drop patch

--- packages/mbedtls.cmake
+++ packages/mbedtls.cmake
@@ -2,11 +2,9 @@ ExternalProject_Add(mbedtls
     GIT_REPOSITORY https://github.com/Mbed-TLS/mbedtls.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
-    PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/mbedtls-*.patch
     UPDATE_COMMAND ""
     GIT_REMOTE_NAME origin
-    GIT_TAG master
-    GIT_RESET 1ec69067fa1351427f904362c1221b31538c8b57 # v3.5.0
+    GIT_TAG development
     CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
         -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
@@ -18,7 +16,6 @@ ExternalProject_Add(mbedtls
         -DUSE_SHARED_MBEDTLS_LIBRARY=OFF
         -DINSTALL_MBEDTLS_HEADERS=ON
         -DMBEDTLS_FATAL_WARNINGS=OFF
-        -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS} -mpclmul -msse2 -maes' # needed for i686's target
     BUILD_COMMAND ${MAKE} -C <BINARY_DIR>
     INSTALL_COMMAND ${MAKE} -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
@@ -36,7 +33,7 @@ includedir=\${prefix}/include
 
 Name: mbedtls
 Description: mbedtls
-Version: 3.5.0
+Version: ${mbedtls_version}
 Libs: -L\${libdir} -lmbedtls -lmbedx509 -lmbedcrypto
 Libs.private: -lbcrypt -lws2_32
 Cflags: -I\${includedir}
