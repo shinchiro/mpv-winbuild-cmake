5a0a0ac81d2e40ab9e8ccdc614ec01b4369553f3
zhongfly <11155705+zhongfly@users.noreply.github.com>
2023-10-23 21:38:25 +0800

    mpv: remove libplacebo option

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -51,7 +51,6 @@ ExternalProject_Add(mpv
         -Dopenal=enabled
         -Dspirv-cross=enabled
         -Dvulkan=enabled
-        -Dlibplacebo=enabled
         -Dvapoursynth=enabled
         -Degl-angle=enabled
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
