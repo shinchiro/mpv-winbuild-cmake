78d080deb0b51f9f66990e4b8bd332466878fcd2
Andarwinux <Andarwinux@users.noreply.github.com>
2023-11-11 00:00:00 +0000

    libarchive: switch to cmake and ninja

--- packages/libarchive.cmake
+++ packages/libarchive.cmake
@@ -6,24 +6,39 @@ ExternalProject_Add(libarchive
         xz
         zlib
         nettle
+        mbedtls
+        libxml2
     GIT_REPOSITORY https://github.com/libarchive/libarchive.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-        --disable-bsdtar
-        --disable-bsdcat
-        --disable-bsdcpio
-        --with-lzo2
-        --without-xml2
-        --without-cng
-        --without-openssl
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
-    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+        -G Ninja
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DCMAKE_FIND_ROOT_PATH=${CMAKE_INSTALL_PREFIX}
+        -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DBUILD_SHARED_LIBS=OFF
+        -DENABLE_ZLIB=ON
+        -DENABLE_MBEDTLS=ON
+        -DENABLE_BZip2=ON
+        -DENABLE_ICONV=ON
+        -DENABLE_LIBXML2=ON
+        -DENABLE_NETTLE=ON
+        -DENABLE_EXPAT=ON
+        -DENABLE_LZO=ON
+        -DENABLE_LZMA=ON
+        -DENABLE_CPIO=OFF
+        -DENABLE_CNG=OFF
+        -DENABLE_CAT=OFF
+        -DENABLE_TAR=OFF
+        -DENABLE_OPENSSL=OFF
+        -DENABLE_WERROR=OFF
+        -DBUILD_TESTING=OFF
+        -DENABLE_TEST=OFF
+        -DWINDOWS_VERSION=WIN10
+        -DCMAKE_BUILD_TYPE=Release
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
