46f9527569160765a881542e270a6ee8e8d1a285
Andarwinux <144242044+Andarwinux@users.noreply.github.com>
2023-11-22 21:19:04 +0000

    libvpx: don't strip libs

--- packages/libvpx.cmake
+++ packages/libvpx.cmake
@@ -22,10 +22,10 @@ ExternalProject_Add(libvpx
         --disable-decode-perf-tests
         --disable-encode-perf-tests
         --as=yasm
+        --enable-debug
         --enable-vp9-highbitdepth
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
-            COMMAND ${EXEC} ${TARGET_ARCH}-ranlib ${MINGW_INSTALL_PREFIX}/lib/libvpx.a
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
