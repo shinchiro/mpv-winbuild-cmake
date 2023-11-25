a27e9969e8af482c84af0bd8d26a1cc1bcb80368
zhongfly <11155705+zhongfly@users.noreply.github.com>
2023-11-25 20:30:13 +0800

    angle-headers: switch to git

--- packages/angle-headers.cmake
+++ packages/angle-headers.cmake
@@ -1,11 +1,18 @@
 ExternalProject_Add(angle-headers
-    URL https://gitlab.com/shinchiro/angle/-/archive/main/angle-main.zip?path=include
+    GIT_REPOSITORY https://github.com/google/angle.git
+    SOURCE_DIR ${SOURCE_LOCATION}
+    GIT_REMOTE_NAME origin
+    GIT_TAG main
+    GIT_CLONE_FLAGS "--sparse --filter=tree:0"
+    GIT_CLONE_POST_COMMAND "sparse-checkout set include/EGL include/KHR"
+    GIT_SUBMODULES ""
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include/EGL ${MINGW_INSTALL_PREFIX}/include/EGL
             COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include/KHR ${MINGW_INSTALL_PREFIX}/include/KHR
-    LOG_DOWNLOAD 1
+    LOG_DOWNLOAD 1 LOG_UPDATE 1
 )
 
+force_rebuild_git(angle-headers)
 cleanup(angle-headers install)
