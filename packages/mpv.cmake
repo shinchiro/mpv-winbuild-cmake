acd223d66586446e2c0127d71fdf9ca71fc40d55
zhongfly <11155705+zhongfly@users.noreply.github.com>
2023-11-28 21:28:39 +0800

    mpv: update mpv.com path in strip-binary

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -62,7 +62,7 @@ ExternalProject_Add_Step(mpv strip-binary
     DEPENDEES build
     ${mpv_add_debuglink}
     COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <BINARY_DIR>/mpv.exe
-    COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <BINARY_DIR>/player/mpv.com
+    COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <BINARY_DIR>/mpv.com
     COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <BINARY_DIR>/libmpv-2.dll
     COMMENT "Stripping mpv binaries"
 )
