3f553433c551f9d598deac24356012dbdf3d5f13
Andarwinux <Andarwinux@users.noreply.github.com>
2023-12-01 00:00:00 +0000

    libplacebo: clean & re-sort dependency

--- packages/libplacebo.cmake
+++ packages/libplacebo.cmake
@@ -1,13 +1,14 @@
-get_property(src_glad TARGET glad PROPERTY _EP_SOURCE_DIR)
 get_property(src_fast_float TARGET fast_float PROPERTY _EP_SOURCE_DIR)
+get_property(src_glad TARGET glad PROPERTY _EP_SOURCE_DIR)
 ExternalProject_Add(libplacebo
     DEPENDS
-        vulkan
-        shaderc
-        lcms2
-        glad
         fast_float
+        glad
+        lcms2
         libdovi
+        shaderc
+        vulkan
+        vulkan-header
         xxhash
     GIT_REPOSITORY https://github.com/haasn/libplacebo.git
     SOURCE_DIR ${SOURCE_LOCATION}
@@ -15,20 +16,26 @@ ExternalProject_Add(libplacebo
     GIT_SUBMODULES ""
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
-    COMMAND bash -c "rm -rf <SOURCE_DIR>/3rdparty/glad"
     COMMAND bash -c "rm -rf <SOURCE_DIR>/3rdparty/fast_float"
-    COMMAND bash -c "ln -s ${src_glad} <SOURCE_DIR>/3rdparty/glad"
+    COMMAND bash -c "rm -rf <SOURCE_DIR>/3rdparty/glad"
     COMMAND bash -c "ln -s ${src_fast_float} <SOURCE_DIR>/3rdparty/fast_float"
+    COMMAND bash -c "ln -s ${src_glad} <SOURCE_DIR>/3rdparty/glad"
     COMMAND ${EXEC} CONF=1 meson <BINARY_DIR> <SOURCE_DIR>
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --libdir=${MINGW_INSTALL_PREFIX}/lib
         --cross-file=${MESON_CROSS}
         --default-library=static
-        -Ddebug=true
+        --libdir=${MINGW_INSTALL_PREFIX}/lib
+        --prefix=${MINGW_INSTALL_PREFIX}
         -Db_ndebug=true
+        -Ddebug=true
         -Doptimization=3
-        -Dvulkan-registry='${MINGW_INSTALL_PREFIX}/share/vulkan/registry/vk.xml'
+        -Dd3d11=enabled
         -Ddemos=false
+        -Dlcms=enabled
+        -Dlibdovi=enabled
+        -Dopengl=enabled
+        -Dvulkan-registry='${MINGW_INSTALL_PREFIX}/share/vulkan/registry/vk.xml'
+        -Dvulkan=enabled
+        -Dxxhash=enabled
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
