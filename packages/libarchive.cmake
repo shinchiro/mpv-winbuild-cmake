cece151855fcecd168e914cd0b9a4717cc95531d
Andarwinux <Andarwinux@users.noreply.github.com>
2023-12-01 00:00:00 +0000

    libarchive: clean & re-sort dependency

--- packages/libarchive.cmake
+++ packages/libarchive.cmake
@@ -2,12 +2,13 @@ ExternalProject_Add(libarchive
     DEPENDS
         bzip2
         expat
+        libiconv
+        libxml2
         lzo
+        openssl
         xz
         zlib
         zstd
-        openssl
-        libxml2
     GIT_REPOSITORY https://github.com/libarchive/libarchive.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
@@ -19,22 +20,22 @@ ExternalProject_Add(libarchive
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
         -DCMAKE_FIND_ROOT_PATH=${MINGW_INSTALL_PREFIX}
         -DBUILD_SHARED_LIBS=OFF
-        -DENABLE_ZLIB=ON
-        -DENABLE_ZSTD=ON
-        -DENABLE_OPENSSL=ON
+        -DBUILD_TESTING=OFF
         -DENABLE_BZip2=ON
+        -DENABLE_CAT=OFF
+        -DENABLE_CNG=OFF
+        -DENABLE_CPIO=OFF
+        -DENABLE_EXPAT=ON
         -DENABLE_ICONV=ON
         -DENABLE_LIBXML2=ON
-        -DENABLE_EXPAT=ON
-        -DENABLE_LZO=ON
         -DENABLE_LZMA=ON
-        -DENABLE_CPIO=OFF
-        -DENABLE_CNG=OFF
-        -DENABLE_CAT=OFF
+        -DENABLE_LZO=ON
+        -DENABLE_OPENSSL=ON
         -DENABLE_TAR=OFF
-        -DENABLE_WERROR=OFF
-        -DBUILD_TESTING=OFF
         -DENABLE_TEST=OFF
+        -DENABLE_WERROR=OFF
+        -DENABLE_ZLIB=ON
+        -DENABLE_ZSTD=ON
         -DWINDOWS_VERSION=WIN10
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
