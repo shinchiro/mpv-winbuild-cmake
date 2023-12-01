d6b7aae289e1eb11d9452e793fb1f4156a071337
Andarwinux <Andarwinux@users.noreply.github.com>
2023-12-01 00:00:00 +0000

    libjxl: clean & re-sort dependency

--- packages/libjxl.cmake
+++ packages/libjxl.cmake
@@ -2,12 +2,12 @@ configure_file(${CMAKE_CURRENT_SOURCE_DIR}/libjxl.pc.in ${CMAKE_CURRENT_BINARY_D
 get_property(src_libjpeg TARGET libjpeg PROPERTY _EP_SOURCE_DIR)
 ExternalProject_Add(libjxl
     DEPENDS
-        lcms2
-        libpng
-        zlib
-        libjpeg
         brotli
         highway
+        lcms2
+        libjpeg
+        libpng
+        zlib
     GIT_REPOSITORY https://github.com/libjxl/libjxl.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
@@ -25,30 +25,30 @@ ExternalProject_Add(libjxl
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
         -DCMAKE_FIND_ROOT_PATH=${MINGW_INSTALL_PREFIX}
         -DBUILD_SHARED_LIBS=OFF
-        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-        -DJPEGXL_STATIC=ON
         -DBUILD_TESTING=OFF
-        -DJPEGXL_EMSCRIPTEN=OFF
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
         -DJPEGXL_BUNDLE_LIBPNG=OFF
-        -DJPEGXL_ENABLE_TOOLS=OFF
-        -DJPEGXL_ENABLE_VIEWERS=OFF
+        -DJPEGXL_EMSCRIPTEN=OFF
+        -DJPEGXL_ENABLE_AVX512_SPR=ON
+        -DJPEGXL_ENABLE_AVX512_ZEN4=ON
+        -DJPEGXL_ENABLE_AVX512=ON
+        -DJPEGXL_ENABLE_BENCHMARK=OFF
+        -DJPEGXL_ENABLE_DEVTOOLS=OFF
         -DJPEGXL_ENABLE_DOXYGEN=OFF
         -DJPEGXL_ENABLE_EXAMPLES=OFF
-        -DJPEGXL_ENABLE_MANPAGES=OFF
         -DJPEGXL_ENABLE_JNI=OFF
-        -DJPEGXL_ENABLE_SKCMS=OFF
+        -DJPEGXL_ENABLE_MANPAGES=OFF
         -DJPEGXL_ENABLE_PLUGINS=OFF
-        -DJPEGXL_ENABLE_DEVTOOLS=OFF
-        -DJPEGXL_ENABLE_BENCHMARK=OFF
         -DJPEGXL_ENABLE_SJPEG=OFF
-        -DJPEGXL_ENABLE_AVX512=ON
-        -DJPEGXL_ENABLE_AVX512_ZEN4=ON
-        -DJPEGXL_ENABLE_AVX512_SPR=ON
-        -DJPEGXL_FORCE_SYSTEM_LCMS2=ON
+        -DJPEGXL_ENABLE_SKCMS=OFF
+        -DJPEGXL_ENABLE_TOOLS=OFF
+        -DJPEGXL_ENABLE_VIEWERS=OFF
         -DJPEGXL_FORCE_SYSTEM_BROTLI=ON
         -DJPEGXL_FORCE_SYSTEM_HWY=ON
-        -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -msse2 ${libjxl_unaligned_vector}'
+        -DJPEGXL_FORCE_SYSTEM_LCMS2=ON
+        -DJPEGXL_STATIC=ON
         -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS}     -msse2 ${libjxl_unaligned_vector}'
+        -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -msse2 ${libjxl_unaligned_vector}'
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
             COMMAND bash -c "cp ${CMAKE_CURRENT_BINARY_DIR}/libjxl.pc ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/libjxl.pc"
