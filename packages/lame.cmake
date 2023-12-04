7f8e81b54bfe9270179261d4baffc0f6e1dfc1e3
Andarwinux <Andarwinux@users.noreply.github.com>
2023-12-04 00:00:00 +0000

    lame: exec configure directly

--- packages/lame.cmake
+++ packages/lame.cmake
@@ -11,14 +11,13 @@ ExternalProject_Add(lame
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
     PATCH_COMMAND chmod 755 ${DEBPATCH} && ${DEBPATCH}
-    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
+    CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
         --disable-frontend
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
-    BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
