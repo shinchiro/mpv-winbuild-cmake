82c7ba145b30943170f7bced5b7011f0cd198723
shinchiro <shinchiro@users.noreply.github.com>
2023-12-09 19:30:44 +0800

    ffmpeg: fix configure error on gcc

--- packages/libjxl.cmake
+++ packages/libjxl.cmake
@@ -49,6 +49,7 @@ ExternalProject_Add(libjxl
         -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -msse2 ${libjxl_unaligned_vector}'
         -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS}     -msse2 ${libjxl_unaligned_vector}'
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+          COMMAND sed -i "s/Requires.private:/Requires.private: lcms2/g" <BINARY_DIR>/lib/libjxl_cms.pc
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
