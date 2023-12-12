62912baea14bc9b3d4b68edc15e9b54b0dd8ac03
Andarwinux <Andarwinux@users.noreply.github.com>
2023-12-12 00:00:00 +0000

    libssh: drop patch,use custom pkgconfig

--- packages/libssh.cmake
+++ packages/libssh.cmake
@@ -1,3 +1,4 @@
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/libssh.pc.in ${CMAKE_CURRENT_BINARY_DIR}/libssh.pc @ONLY)
 ExternalProject_Add(libssh
     DEPENDS
         zlib
@@ -6,7 +7,6 @@ ExternalProject_Add(libssh
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
-    PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/libssh-*.patch
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
@@ -14,13 +14,12 @@ ExternalProject_Add(libssh
         -DCMAKE_FIND_ROOT_PATH=${MINGW_INSTALL_PREFIX}
         -DBUILD_SHARED_LIBS=OFF
         -DWITH_ZLIB=ON
-        -DWITH_OPENSSL=ON
-        -DMBEDTLS_INCLUDE_DIR=${MINGW_INSTALL_PREFIX}/include
         -DBUILD_STATIC_LIB=ON
         -DWITH_EXAMPLES=OFF
         -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS} -DHAVE_COMPILER__FUNC__=1'
     BUILD_COMMAND ${MAKE} -C <BINARY_DIR>
     INSTALL_COMMAND ${MAKE} -C <BINARY_DIR> install
+            COMMAND bash -c "cp ${CMAKE_CURRENT_BINARY_DIR}/libssh.pc ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/libssh.pc"
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
