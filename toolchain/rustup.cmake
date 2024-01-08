cfd2a45a56090f136b37bd4dd175dc744b18395f
Andarwinux <Andarwinux@users.noreply.github.com>
2024-01-08 00:00:00 +0000

    rustup: switch to nightly channel
    1. unlock -Z option support.
    2. download rust-src, for build rust stdlib from source.
    3. enable parallel-rustc, for faster compile.
    4. libdovi,rav1e: enable build-std, use gnullvm target for clang.
    
    This will unlock aarch64 support.
    build-std is for unlocking gnullvm target,
    but gnu target can also use this to get latest std and some linker opt from gcc/clang wrapper for free.

--- toolchain/rustup.cmake
+++ toolchain/rustup.cmake
@@ -4,8 +4,9 @@ ExternalProject_Add(rustup
     SOURCE_DIR rustup-prefix/src
     CONFIGURE_COMMAND ${EXEC} CONF=1
         curl -sSf https://sh.rustup.rs |
-        sh -s -- -y --default-toolchain stable --target x86_64-pc-windows-gnu,i686-pc-windows-gnu --no-modify-path --profile minimal
+        sh -s -- -y --default-toolchain nightly --no-modify-path --profile minimal
     BUILD_COMMAND ${EXEC} rustup update
+          COMMAND ${EXEC} rustup component add rust-src
     INSTALL_COMMAND ${EXEC} cargo install cargo-c --profile=release-strip --features=vendored-openssl
     LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
