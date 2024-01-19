a8c10ee679150e0c130f1054b31b530488d8881c
Andarwinux <Andarwinux@users.noreply.github.com>
2024-01-19 00:00:00 +0000

    mesa: misc
    enable Dozen vulkan driver
    add WPD CFI workaround
    fix GIT_REMOTE_NAME & DEPENDS

--- packages/mesa.cmake
+++ packages/mesa.cmake
@@ -1,8 +1,13 @@
 ExternalProject_Add(mesa
+    DEPENDS
+        libva
+        zlib
+        zstd
     GIT_REPOSITORY https://gitlab.freedesktop.org/mesa/mesa.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     GIT_TAG main
+    GIT_REMOTE_NAME origin
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
@@ -14,28 +19,32 @@ ExternalProject_Add(mesa
         "-Dcpp_args='-D__REQUIRED_RPCNDR_H_VERSION__=475'"
         -Db_lto=true
         ${mpv_lto_mode}
-        -Dshared-glapi=disabled
-        -Degl=disabled
-        -Dgles1=disabled
-        -Dgles2=disabled
-        -Dgallium-opencl=disabled
-        -Dmicrosoft-clc=disabled
-        -Dllvm=disabled
-        -Dgallium-xa=disabled
-        -Denable-glcpp-tests=false
-        -Dopengl=false
-        -Dosmesa=false
         -Dbuild-tests=false
         -Dcpp_rtti=false
-        -Dgallium-drivers=d3d12
-        -Dgallium-va=enabled
+        -Degl=disabled
+        -Denable-glcpp-tests=false
         -Dgallium-d3d12-video=true
-        -Dvideo-codecs=h264dec,h264enc,h265dec,h265enc,vc1dec,av1dec,av1enc,vp9dec
-        -Dmin-windows-version=10
-        -Dvulkan-drivers=
+        -Dgallium-drivers=d3d12
+        -Dgallium-opencl=disabled
+        -Dgallium-va=enabled
+        -Dgallium-xa=disabled
+        -Dgles1=disabled
+        -Dgles2=disabled
+        -Dlibunwind=disabled
+        -Dllvm=disabled
+        -Dmicrosoft-clc=disabled
+        -Dmin-windows-version=11
+        -Dopengl=false
+        -Dosmesa=false
+        -Dshared-glapi=disabled
+        -Dspirv-to-dxil=false
         -Dtools=
-    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
-    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
+        -Dvideo-codecs=all
+        -Dvulkan-drivers=microsoft-experimental
+        -Dzlib=enabled
+        -Dzstd=enabled
+    BUILD_COMMAND ${EXEC} WPD=0 CFI=0 ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} WPD=0 CFI=0 ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
