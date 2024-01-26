983cb2f8ccc0b932f2f4c1b53d63b9685edbf4bf
Andarwinux <Andarwinux@users.noreply.github.com>
2024-01-26 00:00:00 +0000

    llvm: update to 18.1

--- toolchain/llvm/llvm.cmake
+++ toolchain/llvm/llvm.cmake
@@ -1,11 +1,11 @@
-set(clang_version "17")
+set(clang_version "18")
 ExternalProject_Add(llvm
     GIT_REPOSITORY https://github.com/llvm/llvm-project.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
     GIT_REMOTE_NAME origin
-    GIT_TAG release/17.x
+    GIT_TAG release/18.x
     LIST_SEPARATOR ,
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR>/llvm -B<BINARY_DIR>
         -G Ninja
@@ -14,10 +14,9 @@ ExternalProject_Add(llvm
         -DCMAKE_C_COMPILER=clang
         -DCMAKE_CXX_COMPILER=clang++
         -DLLVM_ENABLE_ASSERTIONS=OFF
-        -DLLVM_ENABLE_PROJECTS='clang,lld,polly'
+        -DLLVM_ENABLE_PROJECTS='clang,lld'
         -DLLVM_TARGETS_TO_BUILD='X86,NVPTX'
         -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON
-        -DLLVM_POLLY_LINK_INTO_TOOLS=ON
         -DLLVM_ENABLE_LIBCXX=ON
         -DLLVM_ENABLE_LLD=ON
         -DLLVM_INCLUDE_TESTS=OFF
@@ -33,7 +32,7 @@ ExternalProject_Add(llvm
         -DLLVM_LINK_LLVM_DYLIB=ON
         -DLLVM_ENABLE_LIBXML2=OFF
         -DLLVM_ENABLE_TERMINFO=OFF
-        -DLLVM_TOOLCHAIN_TOOLS='llvm-as,llvm-ar,llvm-ranlib,llvm-objdump,llvm-rc,llvm-cvtres,llvm-nm,llvm-strings,llvm-readobj,llvm-dlltool,llvm-pdbutil,llvm-objcopy,llvm-strip,llvm-cov,llvm-profdata,llvm-addr2line,llvm-symbolizer,llvm-windres,llvm-ml,llvm-readelf,llvm-size,llvm-config'
+        -DLLVM_TOOLCHAIN_TOOLS='llvm-ar,llvm-ranlib,llvm-objdump,llvm-rc,llvm-cvtres,llvm-nm,llvm-strings,llvm-readobj,llvm-dlltool,llvm-pdbutil,llvm-objcopy,llvm-strip,llvm-cov,llvm-profdata,llvm-addr2line,llvm-symbolizer,llvm-windres,llvm-ml,llvm-readelf,llvm-size,llvm-config'
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
