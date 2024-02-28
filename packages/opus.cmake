7dcb75867c886cc860128840986b92b894a7e54c
shinchiro <shinchiro@users.noreply.github.com>
2024-02-28 21:27:46 +0800

    opus: switch to main branch

--- packages/opus.cmake
+++ packages/opus.cmake
@@ -2,6 +2,8 @@ ExternalProject_Add(opus
     GIT_REPOSITORY https://github.com/xiph/opus.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_REMOTE_NAME origin
+    GIT_TAG main
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
