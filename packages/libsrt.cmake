d24ccf81fe385fa329eb27a6ee55f9cad1a6d2c3
zhongfly <11155705+zhongfly@users.noreply.github.com>
2024-03-08 07:53:28 +0800

    libsrt: reset to working commit

--- packages/libsrt.cmake
+++ packages/libsrt.cmake
@@ -4,6 +4,9 @@ ExternalProject_Add(libsrt
     GIT_REPOSITORY https://github.com/Haivision/srt.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_REMOTE_NAME origin
+    GIT_TAG master
+    GIT_RESET 618ddfe
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
