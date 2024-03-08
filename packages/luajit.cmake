ab185fc74a92fde34b838d91a4d13bed3c9110bb
Andarwinux <Andarwinux@users.noreply.github.com>
2024-03-08 00:00:00 +0000

    luajit: switch to cmake fork
    without any patches and retarded build system bullshit

--- packages/luajit.cmake
+++ packages/luajit.cmake
@@ -1,58 +1,34 @@
-if(CYGWIN OR MSYS)
-    # It's much easier to just use the target CC on Cygwin than to worry about
-    # pointer size mismatches
-    set(LUAJIT_HOST_GCC ${TARGET_ARCH}-gcc)
-else()
-    set(LUAJIT_HOST_GCC gcc)
-endif()
-
-if(${TARGET_CPU} MATCHES "i686")
-    set(LUAJIT_GCC_ARGS "-m32")
-    set(DISABLE_JIT "-DLUAJIT_DISABLE_JIT")
-else()
-    set(LUAJIT_GCC_ARGS "-m64")
-endif()
-
-set(EXPORT
-    "CROSS=${TARGET_ARCH}-
-    TARGET_SYS=Windows
-    BUILDMODE=static
-    FILE_T=luajit.exe
-    CFLAGS='-DUNICODE'
-    XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT ${DISABLE_JIT}'
-    PREFIX=${MINGW_INSTALL_PREFIX} Q="
-)
-
-# luajit ships with a broken pkg-config file
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/luajit.pc.in ${CMAKE_CURRENT_BINARY_DIR}/luajit.pc @ONLY)
-
 ExternalProject_Add(luajit
     DEPENDS
         libiconv
-    GIT_REPOSITORY https://github.com/openresty/luajit2.git
+    GIT_REPOSITORY https://github.com/Wohlsoft/LuaJIT.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     GIT_REMOTE_NAME origin
-    GIT_TAG v2.1-agentzh
-    PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/luajit-*.patch
+    GIT_TAG v2.1
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${MAKE} -C <SOURCE_DIR>/src
-        "HOST_CC='${LUAJIT_HOST_GCC} ${LUAJIT_GCC_ARGS}'"
-        ${EXPORT}
-        amalg
-    INSTALL_COMMAND ${MAKE}
-        "HOST_CC='${LUAJIT_HOST_GCC} ${LUAJIT_GCC_ARGS}'"
-        ${EXPORT}
-        install
-    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+        -G Ninja
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DCMAKE_FIND_ROOT_PATH=${MINGW_INSTALL_PREFIX}
+        -DBUILD_SHARED_LIBS=OFF
+        -DCMAKE_CROSSCOMPILING=ON
+        -DCMAKE_UNITY_BUILD=ON
+        -DUNITY_BUILD_BATCH_SIZE=0
+        -DCMAKE_UNITY_BUILD_BATCH_SIZE=0
+        -DLUAJIT_BUILD_TOOL=OFF
+        -DLJ_ENABLE_LARGEFILE=ON
+        -DCMAKE_SYSTEM_VERSION=10
+        -DLUAJIT_FORCE_UTF8_FOPEN=ON
+        -DCMAKE_C_FLAGS='-DLUAJIT_ENABLE_LUA52COMPAT'
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
+            COMMAND bash -c "cp ${CMAKE_CURRENT_BINARY_DIR}/luajit.pc ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/luajit.pc"
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-ExternalProject_Add_Step(luajit install-pc
-    DEPENDEES install
-    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/luajit.pc ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/luajit.pc
-)
-
 force_rebuild_git(luajit)
-cleanup(luajit install-pc)
+cleanup(luajit install)
