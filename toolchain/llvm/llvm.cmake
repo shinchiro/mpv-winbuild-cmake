eb3b4388b18ed1d12c22db88ad01d2ce46d68c61
Andarwinux <Andarwinux@users.noreply.github.com>
2024-03-24 00:00:00 +0000

    llvm: set LLVM_THINLTO_CACHE_PATH to CMAKE_INSTALL_PREFIX

--- toolchain/llvm/llvm.cmake
+++ toolchain/llvm/llvm.cmake
@@ -145,6 +145,7 @@ ExternalProject_Add(llvm
         -DLLVM_TOOL_VERIFY_USELISTORDER_BUILD=OFF
         -DLLVM_TOOL_VFABI_DEMANGLE_FUZZER_BUILD=OFF
         -DLLVM_TOOL_XCODE_TOOLCHAIN_BUILD=OFF
+        "-DLLVM_THINLTO_CACHE_PATH='${CMAKE_INSTALL_PREFIX}/llvm-thinlto'"
         "-DCMAKE_C_FLAGS='-g0 -ftls-model=local-exec ${llvm_lto} ${llvm_pgo}'"
         "-DCMAKE_CXX_FLAGS='-g0 -ftls-model=local-exec ${llvm_lto} ${llvm_pgo}'"
         "-DCMAKE_EXE_LINKER_FLAGS='-fuse-ld=lld -Xlinker -s -Xlinker --icf=all -Xlinker --thinlto-cache-policy=cache_size_bytes=1g:prune_interval=1m'"
