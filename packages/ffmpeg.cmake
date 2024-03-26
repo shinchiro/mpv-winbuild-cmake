bf24efb48462f00b738ae564df28f4c5ab39b5d8
Andarwinux <Andarwinux@users.noreply.github.com>
2024-03-26 00:00:00 +0000

    ffmpeg: use svtav1 instead of rav1e
    This makes libdovi the only rust dependency, so it avoids rust stdlib duplicate symbol problem once and for all and improves AV1 encoding performance
    
    fixes #687

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -38,7 +38,7 @@ ExternalProject_Add(ffmpeg
         libzvbi
         libaribcaption
         aom
-        rav1e
+        svtav1
         dav1d
         vapoursynth
         uavs3d
@@ -87,7 +87,7 @@ ExternalProject_Add(ffmpeg
         --enable-libx264
         --enable-libx265
         --enable-libaom
-        --enable-librav1e
+        --enable-libsvtav1
         --enable-libdav1d
         --enable-libdavs2
         --enable-libuavs3d
