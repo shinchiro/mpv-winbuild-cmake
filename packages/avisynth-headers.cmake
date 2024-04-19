d69444c59c5bbfe61e0da4b0af94fa779a3a77a5
Andarwinux <Andarwinux@users.noreply.github.com>
2024-04-19 00:00:00 +0000

    avisynth-headers: switch to ninja

--- packages/avisynth-headers.cmake
+++ packages/avisynth-headers.cmake
@@ -6,15 +6,16 @@ ExternalProject_Add(avisynth-headers
     GIT_SUBMODULES ""
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+        -G Ninja
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
         -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
         -DCMAKE_FIND_ROOT_PATH=${MINGW_INSTALL_PREFIX}
         -DBUILD_SHARED_LIBS=OFF
         -DHEADERS_ONLY=ON
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ${MAKE} VersionGen install
-    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_INSTALL 1
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR> VersionGen
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 force_rebuild_git(avisynth-headers)
 cleanup(avisynth-headers install)
