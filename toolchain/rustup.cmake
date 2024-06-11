4b0954fa2700f0f02c716956da33fb1c16cabc3f
Andarwinux <144242044+Andarwinux@users.noreply.github.com>
2024-06-11 16:58:52 +0000

    rustup: unset LD_PRELOAD
    rustc doesn't support override malloc
    ref: https://github.com/rust-lang/rust/issues/107410
    fixes: #682

--- toolchain/rustup.cmake
+++ toolchain/rustup.cmake
@@ -6,7 +6,7 @@ ExternalProject_Add(rustup
         curl -sSf https://sh.rustup.rs |
         sh -s -- -y --default-toolchain stable --target x86_64-pc-windows-gnu,i686-pc-windows-gnu --no-modify-path --profile minimal
     BUILD_COMMAND ${EXEC} rustup update
-    INSTALL_COMMAND ${EXEC} cargo install cargo-c --profile=release-strip --features=vendored-openssl
+    INSTALL_COMMAND ${EXEC} LD_PRELOAD= cargo install cargo-c --profile=release-strip --features=vendored-openssl
     LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
