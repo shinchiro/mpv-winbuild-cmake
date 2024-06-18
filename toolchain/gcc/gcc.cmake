53ac61f5d7547433c25b8f458dc8dc71ff8df273
shinchiro <shinchiro@users.noreply.github.com>
2024-06-18 13:59:32 +0800

    gcc: update to 14.1.1+20240615

--- toolchain/gcc/gcc.cmake
+++ toolchain/gcc/gcc.cmake
@@ -1,9 +1,9 @@
 ExternalProject_Add(gcc
     DEPENDS
         mingw-w64-headers
-    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/13-20240309/gcc-13-20240309.tar.xz
+    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/14-20240615/gcc-14-20240615.tar.xz
     # https://mirrorservice.org/sites/sourceware.org/pub/gcc/snapshots/13-20240309/sha512.sum
-    URL_HASH SHA512=2d1e0374ebdee526f0549319fc9c364968c52a0d4aaa16759f00453cb083fe58d8f463c47d97f3bb74a0a92e251989eb75a50ee5800b4569978c72d25446b44e
+    URL_HASH SHA512=d8d757cfbedb7342443ce8de4439653537c46d25e552d88cea0ba9f7aa43ad14fb2b42a32a1dce5ae4eb2ac3849024f6b4e700f2c39330a00a65caa3f5fe29e7
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
         --target=${TARGET_ARCH}
