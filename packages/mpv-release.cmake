a3f14492c220f319791cd03786622c671f9f1011
Andarwinux <Andarwinux@users.noreply.github.com>
2024-09-08 00:00:00 +0000

    mpv: support aarch64
    Due to symbol conflict issue, mingw-w64 aarch64 build must disable gl support
    
    WoA devices don't have native gl drivers anyway

--- packages/mpv-release.cmake
+++ packages/mpv-release.cmake
@@ -65,7 +65,7 @@ ExternalProject_Add(mpv-release
         -Dspirv-cross=enabled
         -Dvulkan=enabled
         -Dvapoursynth=enabled
-        -Degl-angle=enabled
+        ${mpv_gl}
     BUILD_COMMAND ${EXEC} LTO_JOB=1 ninja -C <BINARY_DIR>
     INSTALL_COMMAND ""
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -52,7 +52,7 @@ ExternalProject_Add(mpv
         -Dspirv-cross=enabled
         -Dvulkan=enabled
         -Dvapoursynth=enabled
-        -Degl-angle=enabled
+        ${mpv_gl}
         -Dc_args='-Wno-error=int-conversion'
     BUILD_COMMAND ${EXEC} LTO_JOB=1 PDB=1 ninja -C <BINARY_DIR>
     INSTALL_COMMAND ""
