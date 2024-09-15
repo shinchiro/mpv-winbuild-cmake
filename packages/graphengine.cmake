102dee4f71866908f1e90599945e8bcaf4be79cb
zhongfly <11155705+zhongfly@users.noreply.github.com>
2024-09-15 08:19:48 +0800

    graphengine: switch to bitbucket

--- packages/graphengine.cmake
+++ packages/graphengine.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add(graphengine
-    GIT_REPOSITORY https://github.com/sekrit-twc/graphengine.git
+    GIT_REPOSITORY https://bitbucket.org/the-sekrit-twc/graphengine.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     GIT_SUBMODULES ""
