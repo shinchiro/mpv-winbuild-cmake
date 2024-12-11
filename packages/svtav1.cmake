3bee7202563aa18a2a257a31b69c031d76b6cb67
shinchiro <shinchiro@users.noreply.github.com>
2024-12-11 22:28:02 +0800

    svtav1: fix clash symbol name with ffmpeg in aarch64

--- packages/svtav1.cmake
+++ packages/svtav1.cmake
@@ -15,6 +15,7 @@ ExternalProject_Add(svtav1
         -DBUILD_ENC=ON
         -DSVT_AV1_LTO=OFF
         -DBUILD_APPS=OFF
+        -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS} -Dav1_cospi_arr_s32_data=svtav1_av1_cospi_arr_s32_data'
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
