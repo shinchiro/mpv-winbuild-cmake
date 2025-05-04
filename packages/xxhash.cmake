55ce4eb7adb65c7265d24c34092f3242129024cb
zhongfly <11155705+zhongfly@users.noreply.github.com>
2025-05-04 19:42:56 +0800

    xxhash: update cmake location

--- packages/xxhash.cmake
+++ packages/xxhash.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(xxhash
     GIT_TAG dev
     UPDATE_COMMAND ""
     GIT_REMOTE_NAME origin
-    CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR>/cmake_unofficial -B<BINARY_DIR>
+    CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR>/build/cmake -B<BINARY_DIR>
         -G Ninja
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
