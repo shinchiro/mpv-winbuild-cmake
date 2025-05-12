da053e1707b01c5ece3bdcbbc10d935b4c4fd0f3
zhongfly <11155705+zhongfly@users.noreply.github.com>
2025-05-12 10:15:55 +0800

    ffmpeg: remove --enable-postproc

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -61,7 +61,6 @@ ExternalProject_Add(ffmpeg
         --enable-runtime-cpudetect
         --enable-gpl
         --enable-version3
-        --enable-postproc
         --enable-avisynth
         --enable-vapoursynth
         --enable-libass
