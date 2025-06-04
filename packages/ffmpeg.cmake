0c1102e4d3bc78b997a1d68cc9d729a357ec41ae
shinchiro <shinchiro@users.noreply.github.com>
2025-06-04 20:53:44 +0800

    ffmpeg: revert to working commit

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -50,6 +50,7 @@ ExternalProject_Add(ffmpeg
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests/ref/fate"
+    GIT_RESET d4556c98f02e4f2d3deb86efeb060ebe4659be96
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/configure
         --cross-prefix=${TARGET_ARCH}-
