b5a433a6821b6637ff1b203d304653336593ed28
shinchiro <shinchiro@users.noreply.github.com>
2025-06-05 07:11:16 +0800

    openssl: use upstream arm64 config
    ref: https://github.com/openssl/openssl/commit/9da1a9c30e105571dc09ad7bcf756872a99027a7

--- packages/openssl.cmake
+++ packages/openssl.cmake
@@ -9,9 +9,7 @@ ExternalProject_Add(openssl
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !test"
     GIT_SUBMODULES ""
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ""
-    COMMAND cp ${CMAKE_CURRENT_SOURCE_DIR}/openssl-arm64.conf <SOURCE_DIR>/Configurations/11-mingw-arm64.conf
-    COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/Configure
+    CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/Configure
         --cross-compile-prefix=${TARGET_ARCH}-
         --prefix=${MINGW_INSTALL_PREFIX}
         --libdir=lib
