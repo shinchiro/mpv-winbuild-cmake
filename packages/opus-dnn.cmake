5034326c6cbd88911d2883673bedadd42c35e222
CharlesMengCA <58993776+CharlesMengCA@users.noreply.github.com>
2025-07-23 10:11:44 -0400

    opus-dnn: fix build
    https://github.com/xiph/opus/commit/44f448b2ffa12c7920f35a34d3dd6a44e784fe66

--- packages/opus-dnn.cmake
+++ packages/opus-dnn.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(opus-dnn
-    URL https://media.xiph.org/opus/models/opus_data-735117b.tar.gz
-    URL_HASH SHA256=8f34305a299183509d22c7ba66790f67916a0fc56028ebd4c8f7b938458f2801
+    URL https://media.xiph.org/opus/models/opus_data-8a07d57c4fce6fb30f23b3e0d264004e04f1d7b421f5392ef61543d021a439af.tar.gz
+    URL_HASH SHA256=8a07d57c4fce6fb30f23b3e0d264004e04f1d7b421f5392ef61543d021a439af
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
