599cc652a311784f1f4b768af0948ea2e3b1daa1
zhongfly <11155705+zhongfly@users.noreply.github.com>
2025-08-13 00:55:00 +0800

    libbluray: switch to meson

--- packages/libbluray.cmake
+++ packages/libbluray.cmake
@@ -2,26 +2,31 @@ ExternalProject_Add(libbluray
     DEPENDS
         libudfread
         freetype2
+        libxml2
     GIT_REPOSITORY https://code.videolan.org/videolan/libbluray.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     GIT_SUBMODULES ""
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
+    CONFIGURE_COMMAND ${EXEC} sed -i [['/find_library/d']] <SOURCE_DIR>/meson.build
+    COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-        --disable-examples
-        --disable-doxygen-doc
-        --disable-bdjava-jar
-        --without-libxml2
-        --without-fontconfig
-        CFLAGS='-Ddec_init=libbluray_dec_init'
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
-    BUILD_IN_SOURCE 1
+        --libdir=${MINGW_INSTALL_PREFIX}/lib
+        --cross-file=${MESON_CROSS}
+        --buildtype=release
+        --default-library=static
+        -Denable_tools=false
+        -Dbdj_jar=disabled
+        -Djava9=false
+        -Dfreetype=enabled
+        -Dlibxml2=enabled
+        "-Dc_args='-Ddec_init=libbluray_dec_init'"
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
+    COMMAND ${EXEC} sed -i [['s/-lbluray/-lbluray -lgdi32/']] ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/libbluray.pc
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(libbluray)
+force_meson_configure(libbluray)
 cleanup(libbluray install)
