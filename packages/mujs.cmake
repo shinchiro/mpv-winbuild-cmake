9c6ad06af917b40b88b6d3cadec12eaa7b9395ce
shinchiro <shinchiro@users.noreply.github.com>
2025-08-13 23:25:40 +0800

    mujs: migrate to new repo
    Close #784

--- packages/mujs.cmake
+++ packages/mujs.cmake
@@ -8,7 +8,7 @@ prefix=${MINGW_INSTALL_PREFIX}
 host=mingw")
 
 ExternalProject_Add(mujs
-    GIT_REPOSITORY https://github.com/ccxvii/mujs.git
+    GIT_REPOSITORY https://codeberg.org/ccxvii/mujs.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/mujs-*.patch
