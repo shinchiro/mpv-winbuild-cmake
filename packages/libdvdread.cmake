a0358903d0b167ca1a5e98ff9a59f23e21cf10cc
zhongfly <11155705+zhongfly@users.noreply.github.com>
2025-08-13 19:48:26 +0800

    libdvdread: switch to meson

--- packages/libdvdread.cmake
+++ packages/libdvdread.cmake
@@ -4,16 +4,19 @@ ExternalProject_Add(libdvdread
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} autoreconf -fi && CONF=1 <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
+    CONFIGURE_COMMAND ${EXEC} sed -i [['/^if import/,/endif/d']] <SOURCE_DIR>/meson.build
+    COMMAND ${EXEC} meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-shared
-        --with-libdvdcss
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
-    BUILD_IN_SOURCE 1
+        --libdir=${MINGW_INSTALL_PREFIX}/lib
+        --cross-file=${MESON_CROSS}
+        --buildtype=release
+        --default-library=static
+        -Dlibdvdcss=enabled
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(libdvdread)
+force_meson_configure(libdvdread)
 cleanup(libdvdread install)
