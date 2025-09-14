9d31a2b66147e7e3fed154ea2e377e4cd9e0ef52
shinchiro <shinchiro@users.noreply.github.com>
2025-09-14 12:49:03 +0800

    openal-soft: fix build on recent compiler version

--- packages/openal-soft.cmake
+++ packages/openal-soft.cmake
@@ -17,7 +17,8 @@ ExternalProject_Add(openal-soft
         -DALSOFT_EXAMPLES=OFF
         -DALSOFT_TESTS=OFF
         -DALSOFT_BACKEND_PIPEWIRE=OFF
-        -DCMAKE_CXX_FLAGS='-I<SOURCE_DIR>/gsl/include'
+        -DCMAKE_C_FLAGS='-include stdlib.h'
+        -DCMAKE_CXX_FLAGS='-I<SOURCE_DIR>/gsl/include -include cstdlib'
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
           COMMAND bash -c "echo 'Libs.private: -lole32 -luuid -lshlwapi' >> <BINARY_DIR>/openal.pc"
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
