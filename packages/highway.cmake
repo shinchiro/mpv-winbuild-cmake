96fa86c31bac837da8198cfaf26c1de373acfcd3
shinchiro <shinchiro@users.noreply.github.com>
2025-09-15 21:04:09 +0800

    highway: back to master

--- packages/highway.cmake
+++ packages/highway.cmake
@@ -2,7 +2,6 @@ ExternalProject_Add(highway
     GIT_REPOSITORY https://github.com/google/highway.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
-    GIT_RESET b1719736356428b6a9c0d3fbabe49cdfb1730207
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
