9f205aad6e5d5d1585c7fcc98b9d31b06826dfdf
shinchiro <shinchiro@users.noreply.github.com>
2025-11-11 22:38:21 +0800

    cmake: fix issue while downloading Modules/ExternalProject
    fix #799

--- cmake/download_externalproject.cmake
+++ cmake/download_externalproject.cmake
@@ -1,24 +1,16 @@
 set(version "v3.31.6")
 
 execute_process(
-    COMMAND mkdir -p cmake
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/cmake/Modules/ExternalProject
 )
 
-if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/modules.tar.gz")
+if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/cmake/Modules/setup_done")
     execute_process(
-        COMMAND curl -sL https://gitlab.kitware.com/cmake/cmake/-/archive/${version}/cmake-${version}.tar.gz?path=Modules/ExternalProject -o modules.tar.gz
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    )
-    execute_process(
-        COMMAND tar -C ${CMAKE_CURRENT_BINARY_DIR}/cmake --strip-components=1 -xf modules.tar.gz
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    )
-endif()
-
-if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/cmake/Modules/ExternalProject.cmake")
-    execute_process(
-        COMMAND curl -sLO https://gitlab.kitware.com/cmake/cmake/-/raw/${version}/Modules/ExternalProject.cmake
+        COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/cmake/Modules/ExternalProject
+        # Download github folder via https://download-directory.github.io/?url=https://github.com/Kitware/CMake/tree/v3.31.6/Modules/ExternalProject
+        COMMAND unzip ${CMAKE_CURRENT_SOURCE_DIR}/cmake/CMake-${version}-Modules-ExternalProject.zip -d ${CMAKE_CURRENT_BINARY_DIR}/cmake/Modules/ExternalProject
+        COMMAND curl -sLO https://github.com/Kitware/CMake/raw/refs/tags/${version}/Modules/ExternalProject.cmake
+        COMMAND touch setup_done
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/cmake/Modules
     )
     execute_process(
