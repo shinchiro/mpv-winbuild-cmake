293c583a15ad9146d1a10239494b11d17b81f1e7
zhongfly <11155705+zhongfly@users.noreply.github.com>
2026-05-13 22:54:01 +0800

    ngtcp2,nghttp3,nghttp2: add ngtcp2,nghttp3,nghttp2

---
+++ packages/nghttp3.cmake
@@ -0,0 +1,27 @@
+ExternalProject_Add(nghttp3
+    GIT_REPOSITORY https://github.com/ngtcp2/nghttp3.git
+    SOURCE_DIR ${SOURCE_LOCATION}
+    GIT_CLONE_FLAGS "--depth=1 --sparse --filter=tree:0"
+    GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests"
+    GIT_SUBMODULES lib/sfparse
+    GIT_TAG main
+    GIT_REMOTE_NAME origin
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ${EXEC} ${CMAKE_COMMAND} -H<SOURCE_DIR> -B<BINARY_DIR>
+        -G Ninja
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DCMAKE_FIND_ROOT_PATH=${MINGW_INSTALL_PREFIX}
+        -DBUILD_SHARED_LIBS=OFF
+        -DENABLE_LIB_ONLY=ON
+        -DENABLE_STATIC_LIB=ON
+        -DENABLE_SHARED_LIB=OFF
+        -DBUILD_TESTING=OFF
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ${CMAKE_COMMAND} --install <BINARY_DIR>
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+force_rebuild_git(nghttp3)
+cleanup(nghttp3 install)
