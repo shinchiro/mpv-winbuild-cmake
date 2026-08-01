35fc777d79a39e3065783055798d9893747fbebd
zhongfly <11155705+zhongfly@users.noreply.github.com>
2026-08-01 15:04:32 +0800

    ffmpeg: drop xvidcore

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -28,7 +28,6 @@ ExternalProject_Add(ffmpeg
         vorbis
         x264
         ${ffmpeg_x265}
-        xvidcore
         libxml2
         libvpl
         libopenmpt
@@ -89,7 +88,6 @@ ExternalProject_Add(ffmpeg
         --enable-libdav1d
         ${ffmpeg_davs2_cmd}
         ${ffmpeg_uavs3d_cmd}
-        --enable-libxvid
         --enable-libzimg
         --enable-openssl
         --enable-libxml2
